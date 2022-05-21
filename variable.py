7f051ab612c6b24674960e2bcb819ff2d3c60d78
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-05-21 13:59:03 -0400

    variables

--- variable.py
+++ variable.py
@@ -1,1 +1,7 @@
 # Variables are like labels for values. We may save a value and assign it a name so that we can go back to it, use it for other purposes, and edit it in the future.
+
+# For example 
+
+score = 433
+
+# here score is variable and equal sign (=) is the assignment operator and 433 is the value.
