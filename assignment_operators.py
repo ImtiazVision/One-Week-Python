3e16cbc0b4ba93f0d159bf0e49f34cae4ac21166
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-05-23 14:34:01 -0400

    assignment_operator

--- assignment_operators.py
+++ assignment_operators.py
@@ -1,9 +1,12 @@
 # initialize the variable prize_money to be $800
 prize_money = 800
-
+print("prize_money", prize_money)
 # adding $80 to the prize_money variable to update it
-prize_money = prize_money + 80 
+prize_money = prize_money + 80  # this yields 880
+print("prize_money", prize_money)
 
 # alternatively we can do the same thing using the += operator
-prize_money += 80 
+prize_money += 80  # this yields 960
+print("prize_money", prize_money)
+
 
