432753cdcb1823bc089e22c977cf42a1db4a31bf
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-05-25 12:58:52 -0400

    magic trick exercise

---
+++ magic_trick.py
@@ -0,0 +1,14 @@
+# we create a variable named secret_number and assign it to any number 
+secret_number = 7
+# Increase secret_number by 1
+secret_number += 1 # output 8
+# we take the updated value of secret_number and double it
+secret_number *= 2 # output 16
+# we take the updated value of secret_number and add 4 to it  
+secret_number += 4 # output 20
+# we take the updated value of secret_number and divide it by 2
+secret_number /= 2 # output 10
+# we take the updated value of secret_number and subtract our original secret number from it
+secret_number -= 7 # output 3
+# print the final value of secret_number
+print ("secret_number is:", secret_number)
