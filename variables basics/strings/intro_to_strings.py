a7cacb83abd6b28d2473853527e3b7087fac8e23
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-05-26 14:20:09 -0400

    strings

--- variables basics/strings/intro_to_strings.py
+++ variables basics/strings/intro_to_strings.py
@@ -1,1 +1,6 @@
-# Strings are a textual datatype and must be wrapped in quotes. They are strings of characters.
+# Strings are a textual datatype and must be wrapped in quotes. They are strings of characters. For example, the string "Hello" is a string of characters.
+
+word = "Hello"
+print(word)
+ig_user = "Igor"
+print(ig_user)
