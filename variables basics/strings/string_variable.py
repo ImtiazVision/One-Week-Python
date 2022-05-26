26975ca051bf8e543e45f6c1c30f270eef41d0a2
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-05-26 14:36:06 -0400

    strings

---
+++ variables basics/strings/string_variable.py
@@ -0,0 +1,13 @@
+# In python, we can change the type of a variable from string to integer,or vice versa. For example:
+
+score = 433
+print(score)
+
+# we change the type of score from integer to string
+score = 'four hundred thirty three'
+print(score)
+
+
+
+# The new updated score is a string.
+print(score)
