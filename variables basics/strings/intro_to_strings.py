e249afbc61c6a385e2919b527dea3bcb7699ecb4
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-05-26 14:26:28 -0400

    strings

--- variables basics/strings/intro_to_strings.py
+++ variables basics/strings/intro_to_strings.py
@@ -2,9 +2,13 @@
 
 word = "Hello"
 print(word)
+print(type(word))
 ig_user = "Igor"
 print(ig_user)
+print(type(ig_user))
 website_url = "https://www.google.com"
 print(website_url)
+print(type(website_url))
 facebook_url = 'https://www.facebook.com'
 print(facebook_url)
+print(type(facebook_url))
