662c05254f0e2b44271933f8f45e243be8214b04
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-05 13:54:24 -0400

    strings

--- strings/string_indexing.py
+++ strings/string_indexing.py
@@ -1,1 +1,10 @@
 # Strings are indexed that means every character in a string has a corresponding index number/positional number starting from 0.
+
+string1 = "Hi there"
+print(string1[0])
+print(string1[1])
+print(string1[3])
+print(string1[4])
+print(string1[5])
+print(string1[6])
+print(string1[7])
