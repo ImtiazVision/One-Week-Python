6dbf9279630c86393a722d9ca10621aa590693d8
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-07 14:46:26 -0400

    string slicing

---
+++ strings/string_slicing.py
@@ -0,0 +1,14 @@
+message = 'Hello' + ' ' + 'World'
+
+print(message[0:1])
+print(message[0:2])
+print(message[0:3])
+print(message[0:4])
+print(message[0:5])
+print(message[0:6])
+print(message[0:7])
+print(message[0:8])
+print(message[0:9])
+print(message[0:10])
+print(message[0:11])
+print(message[0:])
