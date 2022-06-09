485ce1fa6b872bfd62f89a6c0c5de3d2371f630a
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-09 16:44:37 -0400

    range

---
+++ loops I/range.py
@@ -0,0 +1,13 @@
+for num in range (0,10,2):
+  print(num)
+print("********************************************************")
+  
+for i in range(0,22,2):
+  print(i)
+print("********************************************************")
+
+# initializing a variable named count
+count = 20
+while count > 0:
+  print(count)
+  count -=1
