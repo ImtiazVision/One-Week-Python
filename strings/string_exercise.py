9998eff33edb30bb01aea1d8c073d32da0baf1b8
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-09 17:13:57 -0400

    string exercise

--- strings/string_exercise.py
+++ strings/string_exercise.py
@@ -8,3 +8,12 @@ print(full_name)
 # creating an initials variable that holds the first letter of each first and last name
 initials = first[0] + last[0]
 print(initials)
+
+# creating a variable that holds the first character of each first and last name with a period after each name
+
+initials_2 = first[0] + '.' + last[0] + '.'
+print(initials_2)
+
+# creating a 'nickname' variable that holds the first 4 characters of the last name
+nickname = last[0:4]
+print(nickname)
