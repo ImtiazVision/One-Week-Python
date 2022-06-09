45500b2123e05693482f24093f2fdae196c8ebc7
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-09 17:09:12 -0400

    range

--- strings/string_exercise.py
+++ strings/string_exercise.py
@@ -4,3 +4,7 @@ last = "Smith"
 # Concatenate first and last name into a full name with a space in between
 full_name = first + ' ' +  last
 print(full_name)
+
+# creating an initials variable that holds the first letter of each first and last name
+initials = first[0] + last[0]
+print(initials)
