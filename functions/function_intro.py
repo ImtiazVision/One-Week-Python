12afff00a554a7ba24f706647acdca636e0f23fa
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-10 21:41:49 -0400

    function

--- functions/function_intro.py
+++ functions/function_intro.py
@@ -1,1 +1,7 @@
 # Functions are reusable blocks of code that can be called to do something. To execute a function, we use parenthesis
+# these are some examples of functions:
+print()
+type()
+help()
+
+# Arguments are input values that are passed to a function. Argument is a fancy word for inputs. For example, burger(tomato, lettuce,cheese, well_done)
