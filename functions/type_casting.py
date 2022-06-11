244f196f378517e72bd4fe752f025ad9b05e2fac
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-11 00:01:23 -0400

    input function

--- functions/type_casting.py
+++ functions/type_casting.py
@@ -5,3 +5,5 @@ print(type(11))
 print(type(44.55))
 
 print(type("421.6666"))
+
+# converting strings to numbers. The input function always gives us a string output even if the input provided by the user was an int
