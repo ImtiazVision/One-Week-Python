1c1c23b3fb62faa6a4d42c35024c2176165ca129
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-11 11:28:57 -0400

    age calculator

---
+++ functions/f_strings.py
@@ -0,0 +1,1 @@
+# f-strings are a simple technique to construct strings with interpolated expressions. Any code enclosed by curly brackets will be evaluated and the result converted to a string before being placed into the overall string.
