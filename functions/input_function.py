16c02bf822e22b617346c9613588d9220acd19d9
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-10 23:53:27 -0400

    input function

--- functions/input_function.py
+++ functions/input_function.py
@@ -1,1 +1,4 @@
-# The input() function is used to get input from the user. It will prompt the user to enter some input, converts it into a string, and returns it.
+# The input() function is used to get input from the user. It will prompt the user to enter some input, converts it into a string, and returns it. We use a parenthesis with a quotation mark inside the parenthesis. 
+
+color = input("What is your favorite color? ")
+print("Your favorite color is " + color + "!!!")
