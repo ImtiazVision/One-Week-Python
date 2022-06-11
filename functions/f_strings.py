59d78964aedd376d7ab27c5bf1a9b1734776926f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-11 11:33:29 -0400

    age calculator

--- functions/f_strings.py
+++ functions/f_strings.py
@@ -1,1 +1,5 @@
 # f-strings are a simple technique to construct strings with interpolated expressions. Any code enclosed by curly brackets will be evaluated and the result converted to a string before being placed into the overall string.
+
+age = input('How old are you (in years) ?\n')
+age_in_days = float(age) * 365
+print(f'You are {age_in_days} days old!!!')
