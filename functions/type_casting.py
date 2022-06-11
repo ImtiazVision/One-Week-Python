68c8f78ed6d0c82abbc3c0b56193b8fb42b8f90e
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-11 00:05:40 -0400

    input function

--- functions/type_casting.py
+++ functions/type_casting.py
@@ -7,3 +7,12 @@ print(type(44.55))
 print(type("421.6666"))
 
 # converting strings to numbers. The input function always gives us a string output even if the input provided by the user was an int
+
+# Casting type
+
+print(int("42"))
+print(type(float("42.0")))
+print(type(str(42)))
+
+score = '888'
+print(type(int(score)))
