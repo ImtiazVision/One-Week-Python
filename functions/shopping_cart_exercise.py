07b6ad865f3ed633fd956d91ea55e951d07a807b
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-12 14:09:39 -0400

    shopping cart exercise

--- functions/shopping_cart_exercise.py
+++ functions/shopping_cart_exercise.py
@@ -1,2 +1,8 @@
 print('WELCOME TO OUR SHOPPING CART')
 print('***************************')
+item = input('What item are you purchasing?\n')
+price = input(f'What is the price of {item}?\n')
+quantity = input(f'How many {item} are you purchasing?\n')
+
+print(f"You are purchasing {quantity} {item} at ${price} each with a total of ${float(quantity) * float(price)}")
+
