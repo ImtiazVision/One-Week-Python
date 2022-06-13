c98c1adc6236a3275766747825dabbe6c26cbeef
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-13 18:35:41 -0400

    method chaining

--- methods/method_chaining.py
+++ methods/method_chaining.py
@@ -3,3 +3,9 @@ stripped_address = address.strip()
 print(stripped_address)
 formatted_address = stripped_address.lower()
 print(formatted_address)
+
+# alternatively, we can chain the methods 
+
+email = 'RAAAZ@gmail.com   '
+formatted_email = email.strip().lower()
+print(formatted_email)
