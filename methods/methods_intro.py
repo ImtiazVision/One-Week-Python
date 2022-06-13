754ae91eacd1aff2da8977d4947871e585cbe74c
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-13 10:31:31 -0400

    methods

--- methods/methods_intro.py
+++ methods/methods_intro.py
@@ -1,1 +1,10 @@
 # Methods: are part of objects that we can call them on. They are functions that 'live' on objects. They automatically have access to the object they are called on.
+
+# Some common string methods are followings: 
+
+str.capitalize() # Capitalize first letter of string
+str.upper() # Convert string to uppercase
+str.lower() # Convert string to lowercase
+str.find() # Find first occurrence of substring
+str.join() # Join list of strings into one string
+str.split() # Split string into list of substrings
