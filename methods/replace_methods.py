3dc222531e0fe24a1244da3bf23157a2b3983860
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-13 17:19:16 -0400

    replace  methods

--- methods/replace_methods.py
+++ methods/replace_methods.py
@@ -1,1 +1,6 @@
-# str.replace(old, new, [count]) method replaces all occurrences of old in string with new.
+# str.replace(old, new, [count]) method replaces all occurrences of old in string with new. Both the old and new parameters are required.
+
+distance_traveled = '5kilometers 8kilometers 9kilometers 54kilometers'
+
+replaced = distance_traveled.replace('kilometers', 'km')
+print(replaced)
