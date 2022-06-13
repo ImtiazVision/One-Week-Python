f0d558796cf05c94332501183a31869d81e0d3e7
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-13 17:59:52 -0400

    find & count  methods

---
+++ methods/other_string_methods.py
@@ -0,0 +1,11 @@
+# str.find() method finds the first occurrence of the substring in the string.
+
+prices = '$9.99 $12.99 $15.99'
+find_the_element = prices.find('5')
+print(find_the_element)
+
+# str.count() method tells us how many times a substring occurs in the string.
+
+count_str = '$9.99 $15.99 $12.99 $9.99'
+count_the_element = count_str.count('9')
+print(count_the_element)
