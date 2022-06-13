98a9cf69e73717a841250d92bf21276c52cb97ec
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-13 16:58:31 -0400

    strip methods

--- methods/strip_methods.py
+++ methods/strip_methods.py
@@ -1,5 +1,9 @@
 # str.strip() removes the leading and trailing whitespace from a string.
 
-msg = "  Hello World   "
+msg = "  Hello World   " # removes only the leading and trailing whitespace from a string not the spaces from the middle of a string
 strip = msg.strip()
 print(strip)
+
+msg1 = '--------------Hello      World----------'
+strip1 = msg1.strip('-') # removes the '-' from the string 
+print(strip1)
