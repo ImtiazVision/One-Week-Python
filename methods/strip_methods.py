be1f3bde3e42afdc2a769521ab12a0f612b200f2
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-13 17:05:35 -0400

    strip methods

--- methods/strip_methods.py
+++ methods/strip_methods.py
@@ -7,3 +7,16 @@ print(strip)
 msg1 = '--------,,,....------Hello      World--!!!###$$$--------'
 strip1 = msg1.strip('- , . ! # $') # removes the '-' from the string 
 print(strip1)
+
+# lstrip removes the leading whitespace from a string
+
+msg2 = '     hello     ,,,   '
+lstrip = msg2.lstrip() # removes the leading whitespace from a string
+print(lstrip)
+
+
+# rstrip removes the trailing whitespace from a string
+
+msg3 = ' ,,,     hi there     '
+rstrip = msg3.rstrip()
+print(rstrip)
