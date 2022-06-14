d4e896fa3c6a8d3ada5f06c4c761eadc8dbcc1b0
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-14 12:56:48 -0400

    comparison operators

--- booleans/boolean_intro.py
+++ booleans/boolean_intro.py
@@ -2,7 +2,10 @@
 #For example, has the user logged in? Yes or No. If they have logged in, then the boolean is True. If they have not logged in, then the boolean is False. If the user is not logged in, ask them to log in before liking a post or commenting on a post. If the user is logged in and if they already have liked the post, clicking  on the post would lead to unliking the post. 
 
 # Comparison Operators:
+
 # a > b : Truthy if a is greater than b
 # a < b : Truthy if a is less than b
 # a >= b : Truthy if a is greater than or equal to b
 # a <= b : Truthy if a is less than or equal to b
+# a == b : Truthy if a is equal to b
+# a != b : Truthy if a is not equal to b
