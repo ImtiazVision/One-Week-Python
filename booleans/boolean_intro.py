70bc4de7dc3f770a2221c4b12619e10fb7a18a77
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-14 12:46:28 -0400

    boolean

--- booleans/boolean_intro.py
+++ booleans/boolean_intro.py
@@ -1,2 +1,2 @@
-# Boolean is a basic data type in python used to make decisions, compare things, and logical operations. Every boolean is either True or False.
+# Boolean is one of the 4 basic data type in python used to make decisions, compare things, and logical operations. Every boolean is either True or False.
 #For example, has the user logged in? Yes or No. If they have logged in, then the boolean is True. If they have not logged in, then the boolean is False. If the user is not logged in, ask them to log in before liking a post or commenting on a post. If the user is logged in and if they already have liked the post, clicking  on the post would lead to unliking the post. 
