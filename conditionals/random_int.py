f958ac819dec911a786b574294e385795a54bc80
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-15 18:27:35 -0400

    generating random numbers via random

--- conditionals/random_int.py
+++ conditionals/random_int.py
@@ -1,3 +1,9 @@
-import random
-print(random.randint(1,6))
-print(random.randint(1,6))
+# import random
+# print(random.randint(1,6))
+# print(random.randint(1,6))
+
+from random import randint
+print(randint(1,6))
+print(randint(2,11))
+print(randint(3,9))
+
