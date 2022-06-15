a61fbaab26b8334c964cfa446bfea3756c8c44ec
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-15 12:21:29 -0400

    name length exercise

--- conditionals/name_length_exercise.py
+++ conditionals/name_length_exercise.py
@@ -1,12 +1,12 @@
 avg_name_length = 12
 first_name = input("What is your first name?\n")
 last_name = input("What is your last name?\n")
-full_name_char = len(first_name) + len(last_name)
+name_length = len(first_name) + len(last_name)
 full_name = first_name + " " + last_name
 
-if full_name_char < avg_name_length:
+if name_length < avg_name_length:
   print(full_name, 'is shorter than average american name.')
-elif len(full_name_char) > avg_name_length:
+elif len(name_length) > avg_name_length:
   print(full_name, 'is longer than average american name.')
-elif len(full_name_char) == avg_name_length:
+elif len(name_length) == avg_name_length:
   print(full_name, 'is exactly the average length of an american name.')
