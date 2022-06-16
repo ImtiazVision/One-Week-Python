b5eb69a4d9520dc2232e10a1ee2813c6d8aa18b2
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-15 22:02:06 -0400

    tweet checker exercise

--- conditionals/tweet_character_checker.py
+++ conditionals/tweet_character_checker.py
@@ -1,7 +1,8 @@
 tweet = input("Enter a tweet: ")
 tweet_char_count = len(tweet)
+max_tweet_chars = 140
 
-if tweet_char_count > 140:
-    print(f"Tweet is {tweet_char_count - 140} characters too long")
+if tweet_char_count > max_tweet_chars:
+    print(f"Tweet is {tweet_char_count - max_tweet_chars} characters too long")
 else:
   print(f"That {tweet_char_count} characters tweet is fine")
