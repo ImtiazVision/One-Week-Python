96f42e38532afdfbc1d44ca1562f38e739f887c9
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-16 11:26:42 -0400

    nesting conditionals

---
+++ conditionals/nest_conditionals.py
@@ -0,0 +1,10 @@
+unit = 'fahrenheit'
+temp = int(input('Enter the temperature: '))
+
+if unit == 'fahrenheit':
+  if temp <= 32:
+    print('It is freezing outside.')
+  else:
+    print('It is not freezing outside.')
+else:
+  print('I am not too sure about celsius.')
