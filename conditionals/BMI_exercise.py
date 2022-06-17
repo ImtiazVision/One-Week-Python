c79557f918c9f186f8c5a13c9c1d4e39d7bf10d6
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-17 19:36:25 -0400

    bmi exercise

--- conditionals/BMI_exercise.py
+++ conditionals/BMI_exercise.py
@@ -2,18 +2,17 @@ height = float(input("Enter your height in inches: "))
 weight = float(input("Enter your weight in pounds: "))
 
 bmi = (weight * 703) / (height ** 2)
-print("Your BMI is: ", bmi)
 if bmi < 16.0:
-    print("You are severely underweight.")
+    print(f"Your BMI of {bmi} makes you severely underweight.")
 elif bmi < 18.4:
-    print("You are underweight.")
+    print(f"Your BMI of {bmi} makes you underweight.")
 elif bmi < 24.9:
-  print("You are normal.")
+  print(f"Your BMI of {bmi} makes you normal.")
 elif bmi < 29.9:
-  print("You are overweight.")
+  print(f"Your BMI of {bmi} makes you overweight.")
 elif bmi < 34.9:
-  print("You are obese.")
+  print(f"Your BMI of {bmi} makes you obese.")
 elif bmi < 39.9:
-  print("You are severely obese.")
+  print(f"Your BMI of {bmi} makes you obese.")
 else:
-  print("You are morbidly obese.")
+  print(f"Your BMI of {bmi} makes you morbidly obese.")
