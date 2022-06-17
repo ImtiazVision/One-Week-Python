01fa92d0b5e47416cb9917fc799ced38801c490b
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-17 19:29:29 -0400

    bmi exercise

--- conditionals/BMI_exercise.py
+++ conditionals/BMI_exercise.py
@@ -5,5 +5,15 @@ bmi = (weight * 703) / (height ** 2)
 print("Your BMI is: ", bmi)
 if bmi < 16.0:
     print("You are severely underweight.")
-elif bmi > 16.0 and bmi < 18.4:
+elif bmi < 18.4:
     print("You are underweight.")
+elif bmi < 24.9:
+  print("You are normal.")
+elif bmi < 29.9:
+  print("You are overweight.")
+elif bmi < 34.9:
+  print("You are obese.")
+elif bmi < 39.9:
+  print("You are severely obese.")
+else:
+  print("You are morbidly obese.")
