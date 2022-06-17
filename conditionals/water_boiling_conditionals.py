8e2a3420f200945bc95879785e3a77904e78de25
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-17 10:50:29 -0400

    conditionals

--- conditionals/water_boiling_conditionals.py
+++ conditionals/water_boiling_conditionals.py
@@ -1,2 +1,11 @@
+# f - 212
+# c - 100
+# k - 373
 unit = input("Enter the unit of measurement: ")
 temp = input("What is the  temperature of the water: ")
+
+if unit == 'f':
+  if temp == '212':
+    print("Water is boiling")
+  else:
+    print("Water is not boiling. Must hit 212 degrees")
