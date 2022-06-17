cef7e708f9e4d32c8c233acc178bffea156fdbaa
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-17 15:07:02 -0400

    bmi

--- conditionals/BMI_exercise.py
+++ conditionals/BMI_exercise.py
@@ -3,3 +3,7 @@ weight = float(input("Enter your weight in pounds: "))
 
 bmi = (weight * 703) / (height ** 2)
 print("Your BMI is: ", bmi)
+if bmi < 16.0:
+    print("You are severely underweight.")
+elif bmi > 16.0 and bmi < 18.4:
+    print("You are underweight.")
