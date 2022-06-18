d41f2f25651996a8439fcd42207ebac90a4ff256
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-18 14:58:31 -0400

    logical NOT

--- conditionals/logical_NOT.py
+++ conditionals/logical_NOT.py
@@ -1,10 +1,14 @@
 # The 'not' operator changes True to False and False to True. It negates expressions.
 
-from pyrsistent import b
 
 
 birth_year = input("What year was your birth year? ")
+
 if not birth_year.isnumeric():
   birth_year = input("That isn't a number. Try again please! What year was your birth year? ")
+  
 birth_year = int(birth_year)
+
 print(f"You were born {2022 - birth_year} years ago.")
+
+
