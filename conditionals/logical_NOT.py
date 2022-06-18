eb1170db3cdaf5fd27c2eb67a26fed6d75cf5f32
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-18 14:41:57 -0400

    logical NOT

--- conditionals/logical_NOT.py
+++ conditionals/logical_NOT.py
@@ -1,4 +1,4 @@
 # The 'not' operator changes True to False and False to True. It negates expressions.
 
 birth_year = int(input("What year was your birth year? "))
-print(f"You were born in {birth_year}.")
+print(f"You were born {2022 - birth_year} years ago.")
