7caa6a6d1c51819162c3112a4a4d95c10cc77724
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-18 13:27:06 -0400

    logical OR

--- conditionals/logical_OR.py
+++ conditionals/logical_OR.py
@@ -1,1 +1,8 @@
 # The 'or' operator will evaluate to True if one or both of the left or right sides evaluate to True.
+
+day = input("Please enter what day of the week it is : ")
+
+if day == 'saturday' or day == 'sunday':
+    print("It's the weekend!")
+else:
+    print("It's a weekday!")
