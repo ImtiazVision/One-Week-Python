c020c20739db2ab97cf7d1d9b6bd7716c8c1764c
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-18 12:42:49 -0400

    logical AND

--- conditionals/logical_AND.py
+++ conditionals/logical_AND.py
@@ -1,1 +1,7 @@
 # The 'and' operator will evaluate to True only if both the left and right sides evaluate to True.
+
+age = float(input("Enter your age : \n"))
+if age > 10 and age < 25:
+  print("You ar a youngster!")
+else:
+  print("You are not a youngster!")
