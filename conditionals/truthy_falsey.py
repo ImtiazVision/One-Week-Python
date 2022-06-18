2ab1263fd04cd3ee1633f334f5dfd728f43a1598
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-18 15:33:48 -0400

    truthy and falsey value

--- conditionals/truthy_falsey.py
+++ conditionals/truthy_falsey.py
@@ -1,4 +1,10 @@
-color = input("Please enter a color: ")
-# if color != '':
-if color:
-  print(f"{color} is my favorite color too!")
+# color = input("Please enter a color: ")
+# # if color != '':
+# if color:
+#   print(f"{color} is my favorite color too!")
+  
+num_lives = 10
+if num_lives:
+  print("Still playing")
+else:
+  print("Game over")
