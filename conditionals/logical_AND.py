5348daadcc4f0ec817891a2b520320bc4b47092d
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-18 13:12:40 -0400

    logical AND

--- conditionals/logical_AND.py
+++ conditionals/logical_AND.py
@@ -1,4 +1,5 @@
 # The 'and' operator will evaluate to True only if both the left and right sides evaluate to True.
+# nested conditionals are an alternative for logical conditions but logical are shorter and better.  
 
 age = float(input("Enter your age : \n"))
 if age > 10 and age < 25:
