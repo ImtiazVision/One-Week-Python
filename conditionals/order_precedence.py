56af629f98214f644d18f9317a0bff1f515eb320
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-18 22:15:23 -0400

    order precedence logical

--- conditionals/order_precedence.py
+++ conditionals/order_precedence.py
@@ -1,13 +1,22 @@
 #NOT
 #AND
 #OR
+# NOT comes before AND and AND comes before OR
 
 day = "Monday"
 is_vet = False
-age = 54 
+age = 80
 
 # Veterans get a 10% discount
 # teenagers get a 20% discount
 
-if age <= 20 or is_vet and day == "Monday":
-        print("You get in for free today!!!")
+# if age <= 20 or is_vet and day == "Monday":
+    # print("You get in for free today!!!")
+
+# if not (age <= 20 or is_vet and day == "Monday"):
+#         print("You have to buy a ticket!")
+        
+gets_in_for_free = age <= 20 or is_vet and day == "Monday"
+
+if not gets_in_for_free:
+    print("You have to buy a ticket!")
