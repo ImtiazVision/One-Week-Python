08c2807a97366b186bd24e11f282ee00c1ee4137
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-18 22:06:13 -0400

    order precedence logical

--- conditionals/order_precedence.py
+++ conditionals/order_precedence.py
@@ -6,3 +6,8 @@ day = "Monday"
 is_vet = False
 age = 54 
 
+# Veterans get a 10% discount
+# teenagers get a 20% discount
+
+if age <= 20 or is_vet and day == "Monday":
+        print("You get in for free today!!!")
