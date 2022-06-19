5b6223fe921239f50fcc8f2457f3b8146c3926a0
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-19 12:41:16 -0400

    rock paper scissors exercise

---
+++ conditionals/rock_paper_scissors_exercise.py
@@ -0,0 +1,43 @@
+from random import randint
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)"""
+
+paper = """
+    _______
+---'   ____)____
+          ______)
+          _______)
+        _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+# Pick a random number from 1 to 3
+num = randint(1, 3)
+
+# Turn that random number into the computer's RPS move
+
+
+# Ask a user to enter their move
+
+
+# Print the rock, paper, or scissors ASCII art that corresponds to the player's move
+
+
+# Print the rock, paper, or scissors ASCII art that corresponds to the computer's move
+
+
+# Figure out who wins and print the result!
