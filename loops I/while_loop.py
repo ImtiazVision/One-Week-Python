c188d99f700f2fc9dd27d90bc016e2aacb771277
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-20 13:20:57 -0400

    while loop

--- loops I/while_loop.py
+++ loops I/while_loop.py
@@ -1,3 +1,3 @@
-# while loop repeats a block of code as long as the condition is true
+# while loop repeats a block of code as long as the condition is true. While loop is very similar to if statement except that is repeats the code block until the condition is false.
 # while condition:
 #     code block
