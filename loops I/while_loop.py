546ec308663ecfd3d73357d0549addaf63f79c3f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-20 16:53:28 -0400

    while loop

--- loops I/while_loop.py
+++ loops I/while_loop.py
@@ -1,3 +1,16 @@
 # while loop repeats a block of code as long as the condition is true. While loop is very similar to if statement except that is repeats the code block until the condition is false.
 # while condition:
 #     code block
+
+# For example,
+# if x < 10:
+    # print('something')
+# In this case python only checks the above condition once and if it is true it will print the statement.
+# Whereas if we replace "if" with "while" it will repeat the code block until the condition is false.
+
+x = 9
+
+
+while x > 0:
+  print(x)
+  x -= 1 
