ee2e55c0619f051b5ee8afe2e9785ea410f40edd
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-20 12:51:04 -0400

    loop

--- loops I/loops_intro.py
+++ loops I/loops_intro.py
@@ -1,3 +1,5 @@
-# loop repeat stuffs in a set number of times 
+# loop repeat stuffs in a set number of times
+# Example: 
 # "Repeat this code block exactly n times"
 # "Print out every name in the contact list"
+# "Keep playing the game unit a user quits"
