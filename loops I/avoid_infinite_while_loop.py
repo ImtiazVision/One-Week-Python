efebe9be4c5d69918dd3bb39236f51e0d29a2efa
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-21 14:00:36 -0400

    infinite loop

---
+++ loops I/avoid_infinite_while_loop.py
@@ -0,0 +1,5 @@
+count = 8
+
+while count < 16:
+  print(f"Number is {count}")
+  count -= 1 # we are moving further away from the start of the count value 8 like 7 6 5 4 3 2 1 0 -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -12 etc...and it will always be less than 16 because we are moving away from 16.
