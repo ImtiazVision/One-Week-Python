af235af8ad073a2d9af844cf2029193548ce1f2c
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-21 16:49:14 -0400

    while loop

--- loops I/avoid_infinite_while_loop.py
+++ loops I/avoid_infinite_while_loop.py
@@ -1,5 +1,12 @@
-count = 8
+# count = 8
 
-while count < 16:
-  print(f"Number is {count}")
-  count -= 1 # we are moving further away from the start of the count value 8 like 7 6 5 4 3 2 1 0 -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -12 etc...and it will always be less than 16 because we are moving away from 16.
+# while count < 16:
+  # print(f"Number is {count}")
+  # count -= 1 # we are moving further away from the start of the count value 8 like 7 6 5 4 3 2 1 0 -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -12 etc...and it will always be less than 16 because we are moving away from 16.
+  
+  
+count = 10
+
+while count > 0:
+  print(f"Count is  {count}") 
+  count -= 1
