913ff72826e01d80490c412908a6b98e974638d7
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-22 10:08:50 -0400

    for loop

---
+++ loops I/for_loop.py
@@ -0,0 +1,6 @@
+# for loop has a variable and we can name the variable anything we want. It also has an iterable(i.e. strings, lists, tuples) and finally it has a statement that runs. 
+
+word = "Hello, World!"
+
+for var in word:
+  print(var)
