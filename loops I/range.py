7058a8f0a0a836e0f16410eac693a39bfeef2424
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-22 17:32:42 -0400

    range

--- loops I/range.py
+++ loops I/range.py
@@ -1,5 +1,9 @@
 # range returns an object that produces a sequence of integers from start (inclusive) to stop (exclusive) by step. Step specifies the increment/decrement. 
 # Example: range(i, j) produces i, i+1, i+2, i+3, ..., j-1. Start defaults to 0, and stop is omitted. range(4) produces 0, 1, 2, 3. 
+for i in range(10):
+  print(i)
+print("**************************")
+
 for num in range (0,10,2):
   print(num)
 print("********************************************************")
