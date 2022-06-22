50df862e5f29539cf4ee356ad68616d109d76044
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-22 10:00:46 -0400

    snake eyes exercise while loop

--- loops I/snake_eyes_ex.py
+++ loops I/snake_eyes_ex.py
@@ -1,10 +1,14 @@
 from random import randint
 roll_1 = randint(1,6)
 roll_2 = randint(1,6) 
+roll_count = 1
 
 while roll_1 != 1 or roll_2 != 1:
   print(roll_1, roll_2)
   roll_1 = randint(1,6)
   roll_2 = randint(1,6)
+  roll_count += 1
+  
 print(roll_1, roll_2)
 print("Yay, that was a snake eyes !!!")
+print(f"It took {roll_count} rolls to get the snake eyes!")
