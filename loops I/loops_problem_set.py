84ce4aee255de8aeb856d5ce9fbcdf9a1aae0426
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-23 13:52:01 -0400

    while and for loop exercise;

---
+++ loops I/loops_problem_set.py
@@ -0,0 +1,28 @@
+
+#  ----------
+#    PART 1
+#  ----------
+word = "supercalifragilisticexpialidocious"
+
+# Write a for loop that prints out each character in the above "word" variable
+
+for char in word:
+  print(char)
+
+
+# Write a while loop that does the same thing!
+
+
+#  ----------
+#    PART 2
+#  ----------
+# Write a while loop that prints the even numbers from 100 to 140 (including 140)
+
+
+# Write a for loop that does the same thing (with range())
+
+
+#  ----------
+#    PART 3
+#  ----------
+# Write a loop that asks a user to input the passphrase "sillygoose" and keeps asking them to do so until they comply:
