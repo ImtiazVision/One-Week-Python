ff78a3189ab1b6197b76dbb58fc7469cc3976e71
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-23 12:10:39 -0400

    while and for loop exercise;

--- loops I/bottles_of_water.py
+++ loops I/bottles_of_water.py
@@ -12,5 +12,11 @@ print("*******************************")
 # While loop implementation
 num_bottles = 99
 while num_bottles > 0:
-  print(f'{num_bottles} bottles of water left on the wall')
+  print(f'{num_bottles} bottles of water on the wall.')
+  print(f'{num_bottles} bottles of water.')
+  if num_bottles == 1:
+    print(f"Take one down, pass it around, no more bottle of water on the wall.")
+  else:
+    print(f'Take one down, pass it around, {num_bottles - 1} bottles of water on the wall.')
+  print("*" * 50)
   num_bottles -= 1
