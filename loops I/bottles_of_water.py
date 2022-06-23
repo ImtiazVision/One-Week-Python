9806a6c69565fecb4aeea8a9b1fcada3ea44ab14
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-23 11:57:58 -0400

    while and for loop exercise;

--- loops I/bottles_of_water.py
+++ loops I/bottles_of_water.py
@@ -1,6 +1,8 @@
 
 for num_bottles in range(99,0,-1):
-  print(f'{num_bottles} bottles of water on the wall')
+  print(f'{num_bottles} bottles of water on the wall.')
+  print(f'{num_bottles} bottles of water.')
+  print(f'Take one down, pass it around,{num_bottles - 1} bottles of water on the wall.')
 print("*******************************") 
 
 bottle = 99
