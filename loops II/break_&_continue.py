8929dfdd342d1b010018335014af3ba6fb3bae07
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-23 21:55:21 -0400

    break and continue keyword

--- loops II/break_&_continue.py
+++ loops II/break_&_continue.py
@@ -1,5 +1,10 @@
 # we can use the 'break' keyword to prematurely exit a loop. Usually this is done inside of a conditional. 
 
+for char in 'cricketmatch':
+  if char == 'm':
+    break
+  print(char)
+print("End of loop")
 
 
 # The 'continue' keyword end the current iteration of the loop, but does not break out of the loop.
