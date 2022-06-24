0924e44d45b09f603dc9c91a7709a604fadfa5b4
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-23 23:04:23 -0400

    break keyword statement implementation in while loop

--- loops II/break_&_continue.py
+++ loops II/break_&_continue.py
@@ -1,4 +1,7 @@
 # we can use the 'break' keyword to prematurely exit a loop. Usually this is done inside of a conditional. 
+# for loop break implementation
+from email import message
+
 
 for char in 'cricketmatch':
   if char == 'm':
@@ -6,5 +9,14 @@ for char in 'cricketmatch':
   print(char)
 print("End of loop")
 
+# while loop break implementation
+
+message = input("Please greet with a 'hi': ")
+
+while True:
+  if message == 'hi':
+    break
+  message = input("Say hi: ")
+  
 
 # The 'continue' keyword end the current iteration of the loop, but does not break out of the loop.
