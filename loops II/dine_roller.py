cca033cd7d55282ce369601c08614341438c4438
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-25 11:23:37 -0400

    dice roller

--- loops II/dine_roller.py
+++ loops II/dine_roller.py
@@ -1,7 +1,7 @@
 from random import randint
 
-num_dice = int(input("How many dice are we rolling?"))
-num_sides = int(input("How many sides on each die?"))
-input("Roll again? ('q' to quit)")
+num_dice = int(input("How many dice are we rolling? "))
+num_sides = int(input("How many sides on each die? "))
+# input("Roll again? ('q' to quit)")
 
-randint(1, num_sides)
+print(randint(1, num_sides))
