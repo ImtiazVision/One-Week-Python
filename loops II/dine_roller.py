b64db163bb9bb2499c870900531ecc8bd938e1b4
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-25 13:28:47 -0400

    dice rolling

--- loops II/dine_roller.py
+++ loops II/dine_roller.py
@@ -2,6 +2,9 @@ from random import randint
 
 num_dice = int(input("How many dice are we rolling? "))
 num_sides = int(input("How many sides on each die? "))
-# input("Roll again? ('q' to quit)")
+# 
 
-print(randint(1, num_sides))
+while True:
+  input("Roll again? ('q' to quit)")
+  print(randint(1, num_sides))
+
