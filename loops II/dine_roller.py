4b7ac8c1cc00bc35112c0c9156617003fd7b8edd
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-25 11:16:31 -0400

    dice rolling exercise using randint

--- loops II/dine_roller.py
+++ loops II/dine_roller.py
@@ -3,3 +3,5 @@ from random import randint
 num_dice = int(input("How many dice are we rolling?"))
 num_sides = int(input("How many sides on each die?"))
 input("Roll again? ('q' to quit)")
+
+randint(1, num_sides)
