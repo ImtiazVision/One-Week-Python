803ec421149111e78796cee166079ae2e537eaa4
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-27 12:29:36 -0400

    toothpick game

--- loops II/toothpic_game_exercise.py
+++ loops II/toothpic_game_exercise.py
@@ -1,6 +1,8 @@
 # ask for players name
 print("Welcome To The Game!")
 print("**********************")
+player_2_name = input("Enter player 1's name: ")
+player_1_name = input("Enter player 2's name: ")
 # game loop, until someone wins: we 'break' out of the loop
 
 # ask player 1 to enter number of toothpick s/he would take away and subsequently subtract that number from original toothpick number
