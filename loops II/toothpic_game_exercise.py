05c523b4cf58332a691b53733e24d699f32a50d2
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-26 20:08:25 -0400

    toothpick game

---
+++ loops II/toothpic_game_exercise.py
@@ -0,0 +1,13 @@
+# ask for players name
+
+# game loop, until someone wins: we 'break' out of the loop
+
+# ask player 1 to enter number of toothpick s/he would take away and subsequently subtract that number from original toothpick number
+
+
+# check to see if they win, whoever takes the last toothpick will win!
+
+
+# ask player 2 to enter number of toothpick s/he would take away and subsequently subtract that number from original toothpick number 
+
+# check to see if they win, whoever takes the last toothpick will win!
