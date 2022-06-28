5c470e867c72fce6b7df18ff653c0e58e498da1a
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-28 10:21:45 -0400

    toothpick game

--- loops II/toothpic_game_exercise.py
+++ loops II/toothpic_game_exercise.py
@@ -2,8 +2,8 @@
 print("Welcome To The Game!")
 print("**********************")
 num_of_toothpick_remaining = 13
-player_2_name = input("Enter player 1's name: ")
-player_1_name = input("Enter player 2's name: ")
+player_1_name = input("Enter player 1's name: ")
+player_2_name = input("Enter player 2's name: ")
 
 # we are going to ask the user how many toothpicks they take while the left_number is > 0
 # while True:
@@ -11,6 +11,12 @@ print('| ' * num_of_toothpick_remaining)
 print(f'There are {num_of_toothpick_remaining} toothpicks left.')
 player_1_input = int(input(f"{player_1_name}, how many toothpicks do you take? "))
 num_of_toothpick_remaining -= player_1_input
+
+print('| ' * num_of_toothpick_remaining)
+print(f'There are {num_of_toothpick_remaining} toothpicks left.')
+player_2_input = int(
+    input(f"{player_2_name}, how many toothpicks do you take? "))
+num_of_toothpick_remaining -= player_2_input
 # game loop, until someone wins: we 'break' out of the loop
 
 # ask player 1 to enter number of toothpick s/he would take away and subsequently subtract that number from original toothpick number
