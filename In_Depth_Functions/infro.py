2420afe77c600448c0775c1913981407b4b51c92
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-29 14:45:20 -0400

    function

--- In_Depth_Functions/infro.py
+++ In_Depth_Functions/infro.py
@@ -1,1 +1,4 @@
 # Functions are reusable actions that have a name
+# Why we use function:
+# To prevent code duplication and keep code DRY(Don't Repeat Yourself)
+# To abstract away code, break a complex program down into small pieces
