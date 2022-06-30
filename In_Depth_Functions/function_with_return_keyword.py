46c83db15c57bcd6776f3b54bc95d7b59b6222e7
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-30 14:23:16 -0400

    function with return keyword

--- In_Depth_Functions/function_with_return_keyword.py
+++ In_Depth_Functions/function_with_return_keyword.py
@@ -1,1 +1,6 @@
-# The "return" keyword outputs whatever value comes after the "return" keyword. It terminates the execution of a function.
+# The "return" keyword outputs whatever value comes after the "return" keyword. It terminates the execution of a function. It means we can only get a single value from a function. We can have multiple 'return' statements, but only one of them will execute(the first one). 
+
+def multiply(a,b):
+  return a*b
+
+multiply(8,8)
