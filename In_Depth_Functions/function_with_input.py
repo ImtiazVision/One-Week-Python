24a724faa773957ea3b5de920e595aa025e188e8
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-06-30 13:40:47 -0400

    function with an input argument/parameter

---
+++ In_Depth_Functions/function_with_input.py
@@ -0,0 +1,8 @@
+# 'intensity' inside the parenthesis is an 'argument/parameter' which ask the user for an input from their end
+
+def laugh(intensity):
+  print('HA' * intensity)
+
+# here, we pass 8 as an 'argument' so that 'HA' will be printed 8 times and we don't have to hard code   
+laugh(8)
+
