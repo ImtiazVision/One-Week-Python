813c9c1b5c190cc5682741da2ab33650cf4503ff
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-01 16:26:44 -0400

    strip method

--- In_Depth_Functions/function_practice.py
+++ In_Depth_Functions/function_practice.py
@@ -23,10 +23,10 @@
 # result = is_even(9)
 # print(result)
 
-def slugify(phrase):
+# def slugify(phrase):
   # Here, we convert the phrase to lowercase, then remove the preceding and trailing spaces, and finally replace the empty spaces with a dash '-'.
-  return phrase.lower().strip().replace(' ', '-')
-  
-  
-phrase = slugify("   Hello there  WE ARE    HUMAN   ")
-print(phrase)
+  # return phrase.lower().strip().replace(' ', '-')
+    
+# phrase = slugify("   Hello there  WE ARE    HUMAN   ")
+# print(phrase)
+
