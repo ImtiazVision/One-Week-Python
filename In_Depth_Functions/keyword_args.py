271ad67958d4b2eff672e84dd9935c705b91cf49
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-03 13:00:28 -0400

    keword arguments

--- In_Depth_Functions/keyword_args.py
+++ In_Depth_Functions/keyword_args.py
@@ -3,4 +3,4 @@ def get_total(price, quantity=1, tax=0.02, discount=0):
   print(subtotal * (1 + tax))
   
 get_total(8.90, 5, 0.0875, 0.15)
-# Writing out the name of the parameter and its corresponding value is known as keyword argument when we call the function. It is similar to default parameter except that default parameter is defined at the beginning of the function while keyword arguments are invoked during the function call at the end of the function. 
+# Writing out the name of the parameter and its corresponding value is known as keyword argument when we call the function. This syntax of 'parameter name = value' is similar to default parameter except that default parameter is defined at the beginning of the function while keyword arguments are invoked during the function call at the end of the function. 
