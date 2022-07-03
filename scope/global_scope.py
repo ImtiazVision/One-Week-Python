e1b1eb9e0c2c3783c173c9a7ac146e7590001beb
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-03 16:27:16 -0400

    scope

--- scope/global_scope.py
+++ scope/global_scope.py
@@ -1,3 +1,4 @@
 # Scope/boundary means where can we reference a given variable 
 # 4 types of scoping
 # Lexical, Enclosing, Global, Built-in (LEGB)
+# Global Scope: Variables declared outside of functions are in the global scope. All functions have access to them.
