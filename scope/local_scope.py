cab93dc682fe3c4f8da322d64fdbb5b68d6ac2a0
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-03 18:52:55 -0400

    local scope

--- scope/local_scope.py
+++ scope/local_scope.py
@@ -1,6 +1,6 @@
 # Local scope: Variables defined in a function are scoped/available  to that function. They are not available outside that function. 
 
 def square(num):
-  ans = num ** 2
+  ans = num ** 2 # here, ans is only available inside square function
   print(ans)
 square(8) 
