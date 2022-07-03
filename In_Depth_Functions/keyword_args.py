4858d397a897cd092890d7984bfddd0008b424f4
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-03 14:13:33 -0400

    keword arguments

--- In_Depth_Functions/keyword_args.py
+++ In_Depth_Functions/keyword_args.py
@@ -5,4 +5,4 @@ def get_total(price, quantity=1, tax=0.02, discount=0):
 get_total(8.90, 5, 0.0875, 0.15)
 # When we call the function, we provide the keyword argument, which is the name of the parameter and its corresponding value. This syntax is similar to the default parameter syntax, except that the default parameter is defined at the beginning of the function where we can optionally put our own value later on, whereas keyword arguments are invoked during function execution by calling it at the end. For keyword arguments, position doesn't matter. 
 
-get_total(price=9.89, discount=0.5, quantity=5, tax=0.05)
+get_total(price=8.90, discount=0.15, quantity=5, tax=0.0875)
