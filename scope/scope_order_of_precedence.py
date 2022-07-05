d81cc41ed93c59f9f8163990854097fa608e4296
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-05 11:31:20 -0400

    scope order of precedence procedure rules

--- scope/scope_order_of_precedence.py
+++ scope/scope_order_of_precedence.py
@@ -1,2 +1,8 @@
 # LEGB 
 # Local > Enclosing/nested inner > Global > Built-in
+
+book = 'fiction'
+def outer():
+  book = 'non-fiction' # because book is re-defined in the function again and local scope take precedence over global scope, 'non-fiction' is printed on the console.
+  print(book)
+outer()
