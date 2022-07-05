d086744bf51ea0895f88e44d752bbd31d3dbe9aa
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-05 11:54:13 -0400

    global scope

--- scope/global_keyword.py
+++ scope/global_keyword.py
@@ -1,2 +1,8 @@
 # 'global' keyword is put in front of a variable so that it is considered as a global variable and accessible as such
 
+def outer():
+  global movie # now we are saying, 'movie' is a global variable that is available outside of this function as it has global scope
+  movie = 'Spider man'
+outer()
+
+print(movie)
