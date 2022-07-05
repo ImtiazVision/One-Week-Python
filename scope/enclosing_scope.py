2d339cdec37b46f8742fa26bdd204dfcecff9fd8
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-04 21:21:09 -0400

    enclosing scope

--- scope/enclosing_scope.py
+++ scope/enclosing_scope.py
@@ -1,2 +1,7 @@
 # Enclosing scope: Nested 'inner' functions have access to variables declared in outer parent functions.
 
+def outside():
+  a = 10
+  def inside():
+    print('a is: ', a)
+  inside()
