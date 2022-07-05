1836c7efd02d1750acba6b23699baaebc527cb71
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-05 11:35:47 -0400

    scope order of precedence procedure rules

--- scope/scope_order_of_precedence.py
+++ scope/scope_order_of_precedence.py
@@ -2,7 +2,16 @@
 # Local > Enclosing/nested inner > Global > Built-in
 
 book = 'fiction'
+
 def outer():
   book = 'non-fiction' # because book is re-defined in the function again and local scope take precedence over global scope, 'non-fiction' is printed on the console.
-  print(book)
+  
+  def inner():
+    print("From inner function :", book)
+  print("From outer function: ",book)
+  
+  inner()
+  
+print("From outside of all function: ", book)
+
 outer()
