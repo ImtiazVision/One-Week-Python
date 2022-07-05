db29f45c917e1ab3ead924264649214fbd22ce1b
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-05 19:55:06 -0400

    lists

--- lists/lists_basics/creating_lists.py
+++ lists/lists_basics/creating_lists.py
@@ -1,1 +1,4 @@
 # Lists are ordered collection of data. They can hold any of the data types such as 'int', 'float', 'boolean', 'string' etc. 
+
+# The most important thing to remember is that list is a ordered collection of data. 
+
