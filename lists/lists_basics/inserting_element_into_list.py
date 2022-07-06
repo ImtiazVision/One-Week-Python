cfb3a25b4732be3c634c69d69b88cb5a45502b9e
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-06 15:47:52 -0400

    insert()

--- lists/lists_basics/inserting_element_into_list.py
+++ lists/lists_basics/inserting_element_into_list.py
@@ -3,5 +3,5 @@
 # element to to inserted into the list 
 
 nums = [8, 89, 22]
-nums.insert(1, 'hello')
-print(nums)
+nums.insert(1, 'hello') # insert 'hello' before the element currently located at index 1 i.e. '89' 
+print(nums)  # [8, 'hello', 89, 22]
