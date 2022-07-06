81ea2f46925568cd0b7727c6c0bdcee883a055f6
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-06 12:57:19 -0400

    appending and extending list

---
+++ lists/lists_basics/appending_and_extending_list.py
@@ -0,0 +1,5 @@
+# 'append' adds a single value to the end of a list
+
+list1 = [1,2,3,4]
+list1.append(5)
+print(list1) # output: list1 = [1,2,3,4,5]
--- lists/lists_basics/updating_list_elements.py
+++ lists/lists_basics/updating_list_elements.py
@@ -1,2 +1,8 @@
 # Updating list elements 
+# we can update a specific element using its index
 
+wish_list = ['apple_watch', 'iPhone', 'necklace']
+
+wish_list[2] = 'iMac'
+
+print(wish_list)
