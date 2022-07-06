6bb21b9b318739269f25c28754694935368a4e5f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-05 20:19:51 -0400

    Accessing Data In Lists

--- lists/lists_basics/accessing_data_in_lists.py
+++ lists/lists_basics/accessing_data_in_lists.py
@@ -3,4 +3,6 @@
 
 # We can retrieve individual elements from a list by passing an index number inside of square brackets. Like strings, indices start at 0.
 
- 
+programming_languages = ['Python', 'Java', 'JavaScript', 'C', 'C#']
+
+print(programming_languages[2])
