8e406affecfc858cf93854084cf67dc63eb2b884
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-06 14:20:42 -0400

    appending and extending list

--- lists/lists_basics/appending_and_extending_list.py
+++ lists/lists_basics/appending_and_extending_list.py
@@ -3,3 +3,9 @@
 list1 = [1,2,3,4]
 list1.append(5)
 print(list1) # output: list1 = [1,2,3,4,5]
+
+# 'extend()' accepts an iterable and appends each item from that iterable to the end of the list
+
+nums = [1,2,3]
+nums.extend('abc')
+print(nums)
