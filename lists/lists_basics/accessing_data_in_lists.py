9eb6b2c7e196a38c76e2308d6d3fdd85b0621f5f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-05 20:17:18 -0400

    Accessing Data In Lists

---
+++ lists/lists_basics/accessing_data_in_lists.py
@@ -0,0 +1,6 @@
+# Accessing Data In Lists
+# list[index]
+
+# We can retrieve individual elements from a list by passing an index number inside of square brackets. Like strings, indices start at 0.
+
+ 
