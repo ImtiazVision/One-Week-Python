a9c13758ee437d33353602b4d0876fed4d5c1ea3
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-06 15:50:03 -0400

    insert()

--- lists/lists_basics/inserting_element_into_list.py
+++ lists/lists_basics/inserting_element_into_list.py
@@ -5,3 +5,5 @@
 nums = [8, 89, 22]
 nums.insert(1, 'hello') # insert 'hello' before the element currently located at index 1 i.e. '89' 
 print(nums)  # [8, 'hello', 89, 22]
+
+# Typically, 'insert()' is used to enter element in the middle of a list or at the beginning of a list
