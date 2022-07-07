e1b89c558c37a731c967fe877b6edf3e78904aa0
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-07 10:42:33 -0400

    slices

--- lists/lists_basics/list_slices.py
+++ lists/lists_basics/list_slices.py
@@ -1,5 +1,5 @@
 # list[start:stop:step]
 
-wish_list = ['house', 'car', 'money', 'peace', 'fame']
+wish_list = ['house', 'car', 'money', 'peace', 'fame', 'Airplane', 'Yacht', 'Palace', 'Rolex', 'Mercedes']
 
 print(wish_list[1:4])  # ['car', 'money', 'peace']
