fb2020b13bdf120e9126e1ac809488fc7b88ac1d
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-08 11:35:10 -0400

    deletion method del statement

--- lists/lists_basics/deletion_methods.py
+++ lists/lists_basics/deletion_methods.py
@@ -20,14 +20,24 @@
 
 # The pop() method removes AND return the last element from a list. pop() is the most common way to remove anything from a list. pop() also accepts a specific index. We can also specify an index like pop(index) if we want to remove a specific element.
 
+# language = ['Python', 'C', 'Java', 'JavaScript', 'C']
+
+# print(language)
+
+# language.pop()
+
+# print(language)
+
+# language.pop(1)
+
+# print(language)
+
+# The 'del' statement (it's not a method!) can be used to delete an item from a specific index in a list
+
 language = ['Python', 'C', 'Java', 'JavaScript', 'C']
 
 print(language)
 
-language.pop()
-
-print(language)
-
-language.pop(1)
+del language[4]
 
 print(language)
