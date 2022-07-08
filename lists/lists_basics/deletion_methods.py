5fab98be7afd7af8398b866343acc129e5aa00ba
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-08 11:21:53 -0400

    deletion method remove(x)

--- lists/lists_basics/deletion_methods.py
+++ lists/lists_basics/deletion_methods.py
@@ -10,10 +10,20 @@
 
 # The remove(x) method will remove the 'First' element in the list that has a value of x
 
+# language = ['Python', 'C', 'Java', 'JavaScript', 'C']
+
+# print(language)
+
+# language.remove('C')
+
+# print(language)
+
+# The pop() method removes the last element from a list. 
+
 language = ['Python', 'C', 'Java', 'JavaScript', 'C']
 
 print(language)
 
-language.remove('C')
+language.pop()
 
 print(language)
