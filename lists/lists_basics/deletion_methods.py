b0f4b38b657991413ed1839bf0be6fb884a9ee1b
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-08 11:17:09 -0400

    deletion method remove(x)

--- lists/lists_basics/deletion_methods.py
+++ lists/lists_basics/deletion_methods.py
@@ -1,9 +1,19 @@
 # clear() method will remove all items from a list
 
-language = ['Python', 'Java', 'JavaScript']
+# language = ['Python', 'Java', 'JavaScript']
+
+# print(language)
+
+# language.clear()
+
+# print(language)
+
+# The remove(x) method will remove the 'First' element in the list that has a value of x
+
+language = ['Python', 'Java', 'JavaScript', 'C']
 
 print(language)
 
-language.clear()
+language.remove('C')
 
 print(language)
