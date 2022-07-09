deab5a04a0949cb34f0d21d5afe441b8dab9f5a4
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-09 13:36:22 -0400

    list loop patterns

--- lists/lists_basics/list_loop_patterns.py
+++ lists/lists_basics/list_loop_patterns.py
@@ -1,9 +1,20 @@
-list1 = [1,2,3,4,5,6,7,8,9,10]
+# list1 = [1,2,3,4,5,6,7,8,9,10]
 
-total = 0
+# total = 0
 
-for element in list1:
-  total = total + element
+# for element in list1:
+#   total = total + element
 
-average = total/ len(list1)
-print(total, average)
+# average = total/ len(list1)
+# print(total, average)
+
+def average(numbers):
+  total = 0
+  for num in numbers:
+    total += num
+  print(total/len(numbers))
+  return total/len(numbers)
+  
+
+
+average([2,4,6,8])
