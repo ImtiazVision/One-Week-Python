07ae2dde4696dbcb9496228fe70b7ee105cde07f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-09 22:07:31 -0400

    list loop patterns

--- lists/lists_basics/list_loop_patterns.py
+++ lists/lists_basics/list_loop_patterns.py
@@ -8,13 +8,23 @@
 # average = total/ len(list1)
 # print(total, average)
 
-def average(numbers):
-  total = 0
-  for num in numbers:
-    total += num
-  print(total/len(numbers))
-  return total/len(numbers)
+# def average(numbers):
+#   total = 0
+#   for num in numbers:
+#     total += num
+#   print(total/len(numbers))
+#   return total/len(numbers)
   
 
 
-average([2,4,6,8])
+# average([2,4,6,8])
+
+list1 = [10, 20, 4,20,5,9, 3, 4, 5,8, 8, 20,3,5, 6, 7, 8, 9, 10]
+
+
+minimum = list1[0]
+
+for element in list1:
+  if element < minimum:
+    minimum = element
+print(minimum)
