0cc8cf09176ccc9248c599c6be5c8958da310124
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-12 15:44:46 -0400

    count method

---
+++ lists/lists_advanced/sort_reverse_count.py
@@ -0,0 +1,6 @@
+# count() method
+# The count() method returns the number of times a value occurs in a list. If the value is not in the list, it returns '0'
+
+language = ['C#', 'C#','C','PHP', 'Python', 'Java', 'JavaScript']
+
+print(language.count('C#'))
