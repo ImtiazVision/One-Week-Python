35be384ceccf9806adba23961443c2ede6b2d046
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-12 10:47:55 -0400

    nested list

--- lists/lists_advanced/nested_lists.py
+++ lists/lists_advanced/nested_lists.py
@@ -1,5 +1,8 @@
 # nested lists are lists inside a list
 
+from xml.sax.saxutils import prepare_input_source
+
+
 list1 = [1, 'hi', True, [], ['hello', False, 4.99]]
 
 multi_dimensional_list = [[[[[]]]]] # total 5 lists nested 
@@ -10,3 +13,16 @@ nums = [1,2,3,4, [5,6]]
 print(nums[4]) # [5,6]
 
 print(nums[4][0]) # [5]
+
+team = [
+  ['John', "Walker"],
+  ['Will', 'Smith'],
+  ["Sam", "Ronald"]
+]
+
+print(team[1][1])
+
+for members in team:
+  # print(members)
+  for player in members:
+    print(player)
