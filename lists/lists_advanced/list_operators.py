2bcbfd7edf242a5b97082d2a43dad1518401381a
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-12 14:59:41 -0400

    list operations addition

--- lists/lists_advanced/list_operators.py
+++ lists/lists_advanced/list_operators.py
@@ -1,6 +1,10 @@
+from audioop import mul
+
+
 list1 = [2,3,4,5]
 list2 = [1,3,5,7]
 
-addition = list1 + list2
+# addition = list1 + list2
+multiplication = list1 * 6
 
-print(addition)
+print(multiplication)
