1c74f98aae0f0894ce48867fcd01938a0725d24e
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-12 15:15:33 -0400

    list operations in operator

--- lists/lists_advanced/list_operators.py
+++ lists/lists_advanced/list_operators.py
@@ -9,3 +9,11 @@ list2 = [1,3,5,7]
 # in operator 
 
 print(3 in list1) # True
+
+ice_cream = ['birthday_cake', 'piece_of_cake', 'butter_pecan', 'strawberry_cheesecake']
+
+user_choice = input("What kind of ice cream would you like?")
+
+while user_choice.lower() not in ice_cream:
+  user_choice = input("Please choose ice cream from the above menu list. What kind of ice cream would you like?")
+print(f"OK, {user_choice} will be served momentarily...")
