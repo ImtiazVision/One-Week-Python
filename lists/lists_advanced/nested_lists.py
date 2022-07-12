71f748791e47434f8b8b12db0d9dd89da8b1ba91
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-12 09:54:14 -0400

    nested list

--- lists/lists_advanced/nested_lists.py
+++ lists/lists_advanced/nested_lists.py
@@ -3,3 +3,10 @@
 list1 = [1, 'hi', True, [], ['hello', False, 4.99]]
 
 multi_dimensional_list = [[[[[]]]]] # total 5 lists nested 
+
+# accessing stuff from a nested list is done by adding [][] subsequently depending on how many nested lists exist
+
+nums = [1,2,3,4, [5,6]]
+print(nums[4]) # [5,6]
+
+print(nums[4][0]) # [5]
