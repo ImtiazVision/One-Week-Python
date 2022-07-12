9cc883885da8e77d486dead04b33995507877ff6
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-12 15:02:40 -0400

    list operations in operator

--- lists/lists_advanced/list_operators.py
+++ lists/lists_advanced/list_operators.py
@@ -5,6 +5,7 @@ list1 = [2,3,4,5]
 list2 = [1,3,5,7]
 
 # addition = list1 + list2
-multiplication = list1 * 6
+# multiplication = list1 * 6
+# in operator 
 
-print(multiplication)
+print(3 in list1) # True
