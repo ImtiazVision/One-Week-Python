04ebd4a88bb4cce29ffbfd3ccad273e3140e915c
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-13 13:18:20 -0400

    sort method

--- lists/lists_advanced/sort_reverse_count.py
+++ lists/lists_advanced/sort_reverse_count.py
@@ -1,6 +1,9 @@
 # count() method
 # The count() method returns the number of times a value occurs in a list. If the value is not in the list, it returns '0'
 
+import numbers
+
+
 language = ['C#', 'C#','C','PHP', 'Python', 'Java', 'JavaScript']
 
 print(language.count('C#'))
@@ -11,3 +14,12 @@ nums = [1,2,3,4,5,6,7,8]
 
 nums.reverse()
 print(nums)
+
+
+# sort() method arrange list elements in an ascending (least to greatest) order
+
+numbers = [8, -2, 4, -9, 3, 1, -4, 5, 4]
+
+numbers.sort()
+
+print(numbers)
