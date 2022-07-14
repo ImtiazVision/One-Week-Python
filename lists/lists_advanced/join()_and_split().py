4f6dc2c6f8ab6358b3e9d7ed359a381b7db56d08
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-14 16:34:30 -0400

    split() method

--- lists/lists_advanced/join()_and_split().py
+++ lists/lists_advanced/join()_and_split().py
@@ -2,4 +2,5 @@
 
 appointment_date = '07/24/2022'
 
-print(appointment_date.split('/'))  # ['07', '24', '2022']
+print(appointment_date.split('/'))  # ['07', '24', '2022']  here, '/' is an argument/delimiter and split() method separate the string based on '/'. Whenever, it finds '/', it split the string and returns them in a list. 
+
