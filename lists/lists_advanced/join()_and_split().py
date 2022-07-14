2a0f4a71e4cbe61f2c776cb6128ea5da0d20dbc6
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-14 16:30:41 -0400

    split() method

---
+++ lists/lists_advanced/join()_and_split().py
@@ -0,0 +1,5 @@
+# split() is a string method that split a string on a given character. It returns a list that holds the split strings. 
+
+appointment_date = '07/24/2022'
+
+print(appointment_date.split('/'))  # ['07', '24', '2022']
