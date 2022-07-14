053ea797fca9696797b7802c40c6fc60d85454d6
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-14 16:54:00 -0400

    join() method

--- lists/lists_advanced/join()_and_split().py
+++ lists/lists_advanced/join()_and_split().py
@@ -4,7 +4,7 @@ appointment_date = '07/24/2022'
 
 print(appointment_date.split('/'))  # ['07', '24', '2022']  here, '/' is an argument/delimiter and split() method separate the string based on '/'. Whenever, it finds '/', it split the string and returns them in a list. 
 
-# join() is a string method that joins together the elements of an iterable/list into a single string. Whatever string we call it on will be used as a separator. 
+# join() is a string method that joins together the elements of an iterable/string/list into a single string. Whatever string we call it on will be used as a separator. 
 
 team_members = ['John', 'Sal', 'Roger', 'Badman']
 
