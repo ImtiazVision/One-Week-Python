fb43055767a019fec181519f358c50f9545d7dfe
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-14 13:44:21 -0400

    == vs is

--- lists/lists_advanced/is_vs_==_compare.py
+++ lists/lists_advanced/is_vs_==_compare.py
@@ -1,10 +1,10 @@
-# We use '==' to compare the contents inside of 2 lists. 
+# We use '==' to compare the contents inside of 2 lists. Do they hold the same values?
 
 print([1,3] == [1,3]) # True
 print([1,3] == [1,3,9]) # False
 print([1,3] == [3,1]) # False
 
-# We use 'is' to compare the identity of 2 lists. 
+# We use 'is' to compare the identity of 2 lists. Are they the same 'container' in memory?
 
 print([3,2,1] is [3,2,1]) # False
 odd = [1,3,5]
