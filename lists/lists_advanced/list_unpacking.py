18b27c5fe000e5a7520423e8eab59c935976578f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-15 11:37:34 -0400

    list unpacking

--- lists/lists_advanced/list_unpacking.py
+++ lists/lists_advanced/list_unpacking.py
@@ -4,6 +4,6 @@ member = ['John', 'Smith', 31]
 
 first, last, age = member
 
-# 
+# In the example above, the first value in the list is stored in a  variable called first, 2nd value as last and 3rd value as age. 
 
 print(first, last, age) 
