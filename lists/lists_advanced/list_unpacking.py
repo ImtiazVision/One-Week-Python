94e81ef38835580dda6983912234d3f72fcd5f11
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-16 12:56:19 -0400

    list unpacking

--- lists/lists_advanced/list_unpacking.py
+++ lists/lists_advanced/list_unpacking.py
@@ -7,3 +7,5 @@ first, last, age = member
 # In the example above, the first value in the list is stored in a  variable called first, 2nd value as last and 3rd value as age. 
 
 print(first, last, age) 
+
+color = [255,43,19]
