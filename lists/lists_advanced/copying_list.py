28962975a74797984e4983562975eb5060e2432c
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-16 15:05:41 -0400

    list copying

--- lists/lists_advanced/copying_list.py
+++ lists/lists_advanced/copying_list.py
@@ -6,3 +6,6 @@ print(list2)
 # list1 and list2 has different id in the memory (2549609097792 and 2549609441536) and thus it is called a 'shallow copy' 
 print(id(list1), id(list2))
 print(list1 is list2) # we can also compare the type(identity) of both list 
+
+# Copying with [:] (slicing)
+# We can also copy lists by creating slices of an entire list. It's not the most readable, but it works!
