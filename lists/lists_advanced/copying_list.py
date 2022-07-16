75d29c6fe94c84ca013b14173633c0366e7c8eff
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-16 14:24:25 -0400

    list copying

--- lists/lists_advanced/copying_list.py
+++ lists/lists_advanced/copying_list.py
@@ -1,5 +1,7 @@
-# The copy() method returns a shallow copy of a list. Nested objects are not copied. 
+# The copy() method returns a 'shallow copy' of a list. Nested objects are not copied. 
 
 list1 = [1,3,5,7,9]
 list2 = list1.copy()
 print(list2)
+# list1 and list2 has different id in the memory (2549609097792 and 2549609441536) and thus it is called a 'shallow copy' 
+print(id(list1), id(list2))
