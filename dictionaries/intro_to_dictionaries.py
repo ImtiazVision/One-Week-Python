7851c93e631b103909dd03fb3c86f4e9b6ddbad5
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-20 16:44:34 -0400

    intro to dictionaries

--- dictionaries/intro_to_dictionaries.py
+++ dictionaries/intro_to_dictionaries.py
@@ -1,1 +1,10 @@
 # Dictionaries allow us to store information in key-value pairs. We use dictionaries when order doesn't matter (list is used when order does matter). 
+
+movie = {
+  "title": "Rogueie",
+  "reviews": 10347,
+  "imdb": 8.5,
+  "runtime": '3h',
+  "year": 2011,
+  "rating": "R"
+}
