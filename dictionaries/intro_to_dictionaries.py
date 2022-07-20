921f8e6b83359e96380f020a6e77073dba448a9b
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-20 16:59:04 -0400

    intro to dictionaries

--- dictionaries/intro_to_dictionaries.py
+++ dictionaries/intro_to_dictionaries.py
@@ -1,5 +1,10 @@
 # Dictionaries allow us to store information in key-value pairs. We use dictionaries when order doesn't matter (list is used when order does matter).
 
+# Dictionaries, known as associative arrays in some other languages, are 'indexed by keys' rather than a numerical index. 
+# - A dictionary holds key-value pairs
+# - Keys can be any immutable data type: numbers, strings, booleans etc.
+# - Values can be anything/whatever we want to input.  
+
 #     Key                Value
 
 #  'address'          '111 Main st'
