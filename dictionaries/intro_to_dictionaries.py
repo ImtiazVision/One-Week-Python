55eab681ef901b3f5a1132a0e839c07a8f0c4046
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-20 16:55:37 -0400

    intro to dictionaries

--- dictionaries/intro_to_dictionaries.py
+++ dictionaries/intro_to_dictionaries.py
@@ -1,4 +1,12 @@
-# Dictionaries allow us to store information in key-value pairs. We use dictionaries when order doesn't matter (list is used when order does matter). 
+# Dictionaries allow us to store information in key-value pairs. We use dictionaries when order doesn't matter (list is used when order does matter).
+
+#     Key                Value
+
+#  'address'          '111 Main st'
+#  'age'                  20
+#   'is_admin'          False
+#     'email'         'someone@email.com'
+
 
 movie = {
   "title": "Rogueie",
