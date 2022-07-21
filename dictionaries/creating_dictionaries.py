4e421dce5f819a5c94104bdb1f84259d5eb6abae
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-21 12:37:40 -0400

    creating dictionaries

--- dictionaries/creating_dictionaries.py
+++ dictionaries/creating_dictionaries.py
@@ -1,3 +1,5 @@
 empty_dictionary = {} # we need curly braces to create a dictionary
 empty_dictionary = dict() # this a built-in dictionary function to create a dictionary just like list() 
 print(type({}))  # <class 'dict'>
+
+order = {"quantity": 5, "price": 5.99} # we separate the elements by coma(,) and separate the key & value by a colon (:)
