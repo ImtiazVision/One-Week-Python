88c4c6e134d47d7b349ee696577127f4bcced365
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-21 12:33:29 -0400

    creating dictionaries

--- dictionaries/creating_dictionaries.py
+++ dictionaries/creating_dictionaries.py
@@ -1,1 +1,3 @@
 empty_dictionary = {} # we need curly braces to create a dictionary
+empty_dictionary = dict() # this a built-in dictionary function to create a dictionary just like list() 
+print(type({}))  # <class 'dict'>
