9999e75c91151fb02705512c6c00b19a85b341de
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-22 10:04:46 -0400

    Accessing Data In Dictionaries

--- dictionaries/accessing_data_in_dictionaries.py
+++ dictionaries/accessing_data_in_dictionaries.py
@@ -1,3 +1,3 @@
 # Retrieving values using dict[key]
 order = {"quantity": 5, "price": 5.99}
-print(order['price'])
+print(order['price']) # we use square bracket for indexing a dictionary while curly bracket for creation of dictionary
