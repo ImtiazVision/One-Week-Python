9171cdb853b8a690a1687f563f4f3115750da656
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-22 09:58:10 -0400

    creating dictionaries

--- dictionaries/creating_dictionaries.py
+++ dictionaries/creating_dictionaries.py
@@ -3,3 +3,7 @@ empty_dictionary = dict() # this a built-in dictionary function to create a dict
 print(type({}))  # <class 'dict'>
 # syntax of a dictionary
 order = {"quantity": 5, "price": 5.99} # we separate the elements by coma(,) and separate the key & value by a colon (:)
+
+# we can't use list [] as a key of a dictionary
+invalid_dict = {[111]: 'list'}
+print(invalid_dict)  # TypeError: unhashable type: 'list'
