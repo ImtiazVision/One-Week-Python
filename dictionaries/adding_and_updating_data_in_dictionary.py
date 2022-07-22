f5553272235c295774a00c45f694ea475bfbe0df
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-22 10:20:13 -0400

    update/add values with dict[key]

--- dictionaries/adding_and_updating_data_in_dictionary.py
+++ dictionaries/adding_and_updating_data_in_dictionary.py
@@ -11,4 +11,8 @@ movie = {
 # to update, we just index the key and set it equal(=) to the updated value
 movie['runtime'] = '4h'
 
+movie['imdb'] += 0.5
+
 print(movie['runtime'])  # 4h
+
+print(movie["imdb"])  # 9.0
