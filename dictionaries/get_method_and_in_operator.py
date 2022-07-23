d345ee3bce8d3ac6355976ad42e80e267c211dda
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-23 15:26:58 -0400

    in operator

--- dictionaries/get_method_and_in_operator.py
+++ dictionaries/get_method_and_in_operator.py
@@ -1,3 +1,7 @@
 # 'in' operator will only look at the 'keys', not the values in a dictionary. The 'in' operator is used to find out whether a key exists in a dictionary or not. 
 
-
+phone_book = {"Batman": 468426,
+              "Cersei": 237734,
+              "Ghostbusters": 44678}
+print("Batman" in phone_book)
+print("Godzilla" in phone_book)
