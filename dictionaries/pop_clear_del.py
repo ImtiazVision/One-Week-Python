fa9f57479d4368d9f1b0b0c618107020ffacaba3
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-23 17:59:19 -0400

    pop method

--- dictionaries/pop_clear_del.py
+++ dictionaries/pop_clear_del.py
@@ -1,1 +1,9 @@
 # The pop() method accepts a key and will delete the corresponding key-value pair in the dictionary. It returns the deleted value. 
+phone_book = {"Batman": 468426,
+              "Cersei": 237734,
+              "Ghostbusters": 44678}
+print(phone_book)
+
+cersei = phone_book.pop("Cersei")
+print(phone_book)
+print(cersei)
