1c5277a3095e934dea305072a88d56f59af77abf
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-24 14:32:39 -0400

    iterating_dictionary_keys_values_items

--- dictionaries/iterating_dictionary_keys_values_items.py
+++ dictionaries/iterating_dictionary_keys_values_items.py
@@ -1,1 +1,9 @@
 # We utilize the dict.items() method to iterate through the dictionary, which converts a dictionary into a collection of (key, value) tuples.
+prices = {
+    'coconut': 5.11,
+    'strawberries': 4.99,
+    'orange': 3.99,
+    'blueberries': 2.99,
+    'blackberries': 4.15,
+    'basil': 2.99
+}
