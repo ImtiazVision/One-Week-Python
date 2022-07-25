4ae97c8e418ff860e9971433edf591cc51197524
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-25 12:21:37 -0400

    import __hello__

--- dictionaries/iterating_dictionary_keys_values_items.py
+++ dictionaries/iterating_dictionary_keys_values_items.py
@@ -26,5 +26,7 @@ prices = {
 # for item in prices.keys():
 #   print(item)
 # here, 'cost' is just a variable name as it could be anything but we made sure that the variable name does makes sense according to our context 
-for cost in prices.values():
-  print(cost)
+# for cost in prices.values():
+#   print(cost)
+
+# import __hello__ # fun fact, this will print 'Hello world!' on the console 
