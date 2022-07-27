5333fc62bd4d0102067d563ba0d67a6403dc62d1
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-27 16:21:00 -0400

    peak dictionary exercise

---
+++ dictionaries/peak_dictionary_exercise.py
@@ -0,0 +1,38 @@
+# The exercise uses the following "peak" dictionary:
+peak = {
+    "name": "Castle Peak",
+    "height": 14264,
+    "summit_log": [],
+    "cell_reception": {
+        "AT&T": "no reception",
+        "T-Mobile": "poor"
+    }
+}
+# Without touching the original variable declaration (above)...
+# Add a "range" key to peak and set it equal to "Elk Mountains"
+
+# Add a "first_climbed" key to peak and set it equal to 1873
+
+# Whoops, there's a mistake with the peak "height".  Update it to 14265
+
+# Add a "Verizon" to the "cell_reception" dict and set it equal to "good"
+
+# You just summited the peak! Add your name to the "summit_log" list
+
+
+# Let's rename "height" to "elevation":
+# Remove "height" from the dict and store the result in a variable.
+
+
+# Use the value for "height" and store it in the dict under they key "elevation"
+
+
+# Loop over the values in the dictionary and print them all out.  Don't ask why, just do it :)
+
+
+# Loop over the keys AND values in the dictionary and print them all out in the following format:
+# key -> value
+# (print an arrow between each pair)
+
+
+# A huge earthquake/meteor/forestfire/tsunami has destroyed the peak.  Please empty out the entire dictionary.
