08e45b0e3c0ea50a377fcf65f5eae270d9e50df9
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-27 17:23:41 -0400

    peak dictionary exercise

--- dictionaries/peak_dictionary_exercise.py
+++ dictionaries/peak_dictionary_exercise.py
@@ -14,7 +14,7 @@ peak['range'] = 'Elk Mountains'
 # Add a "first_climbed" key to peak and set it equal to 1873
 peak['first_climbed'] = 1873
 # Whoops, there's a mistake with the peak "height".  Update it to 14265
-
+peak['height'] = 14265
 # Add a "Verizon" to the "cell_reception" dict and set it equal to "good"
 
 # You just submitted the peak! Add your name to the "summit_log" list
