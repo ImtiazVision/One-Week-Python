e27e19715380354ac60ea4bb293169a6902030e5
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-26 21:48:10 -0400

    nested dictionary

--- dictionaries/dicts_and_lists_combined.py
+++ dictionaries/dicts_and_lists_combined.py
@@ -1,11 +1,11 @@
-prices = {
-  'mango': 2.99,
-  'strawberry': 4.99,
-  'peach': 2.44,
-  'watermelon': 3.11,
-  'coconut': 4.99,
-  'basil': 2.55
-}
+# prices = {
+#   'mango': 2.99,
+#   'strawberry': 4.99,
+#   'peach': 2.44,
+#   'watermelon': 3.11,
+#   'coconut': 4.99,
+#   'basil': 2.55
+# }
 
 # nested dictionary 
 produce = {
