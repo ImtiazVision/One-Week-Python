159b4b0018c6385a61bf1cd7701d8c7eec837ff7
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-26 21:21:38 -0400

    merging 2 dictionaries via update()

--- dictionaries/merging_via_update().py
+++ dictionaries/merging_via_update().py
@@ -7,5 +7,5 @@ phone_book = {"Batman": 468426,
 second_phone_book = {"Cat_woman": 67423, "Jaime": 237734, "Godzilla": 37623}
 
 # Add second phone_book to phone_book
-phone_book.update(second_phone_book)
-print(phone_book)
+# phone_book.update(second_phone_book)
+# print(phone_book)
