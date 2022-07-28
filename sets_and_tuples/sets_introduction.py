d7e5b803217babb14322d27a3af136f3256e9be7
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-28 19:13:37 -0400

    introduction to sets

--- sets_and_tuples/sets_introduction.py
+++ sets_and_tuples/sets_introduction.py
@@ -1,2 +1,8 @@
 # Sets are unordered collections of data with no duplicate(unique) elements. Sets can only contain immutable elements. 
+# We can do the followings with sets:
+# - Add and delete elements
+# - Iterate over elements
+# - Check to see if an element is in a set
+# - Use set operators: union, intersection etc.
+
 
