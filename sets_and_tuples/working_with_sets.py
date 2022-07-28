017cfc9faf274cda31e43990fdcc8ad23d86cbbc
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-28 19:41:30 -0400

    working with  sets

--- sets_and_tuples/working_with_sets.py
+++ sets_and_tuples/working_with_sets.py
@@ -13,8 +13,6 @@ print(languages)
 
 # discard() will remove element just like 'remove()' but won't throw error for missing value (if the value don't exists in a set)
 odd = {9,11,13,15}
-print(9 in odd) # True
-print(19 in odd) # False
 odd.discard(1)
 odd.discard(15)
 print(odd)
@@ -27,4 +25,14 @@ print(languages)
 print({'a','b','c'} is {'a','b','c'}) # 'False' bc they hold different memory container
 
 print({1,2,3} == {1,2,3}) # 'True' bc the elements are same inside the sets ('==' checks whether the contents are same)
+
+# check if a value exists in sets
+odd = {9, 11, 13, 15}
+print(9 in odd)  # True
+print(19 in odd)  # False
+
+# iterate over set
+odd = {9, 11, 13, 15}
+for element in odd:
+  print(element)
 
