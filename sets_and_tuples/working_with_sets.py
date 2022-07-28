aa7c8798994678f2dc0aa22b1b3c5f1daadba8ac
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-28 19:27:11 -0400

    working with  sets

--- sets_and_tuples/working_with_sets.py
+++ sets_and_tuples/working_with_sets.py
@@ -5,3 +5,8 @@ odd.add(9)
 print(odd)
 # we can also find the length of a set using 'len' function
 print(len(odd))
+
+# remove() will remove a single value from a set
+languages = {'Python', "Java", 'JavaScript'}
+languages.remove('Java')
+print(languages)
