89bd2edda044c6e6d17916a22aa9cccaa264a847
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-28 18:57:34 -0400

    why use tuples

---
+++ sets_and_tuples/why_use_tuples.py
@@ -0,0 +1,6 @@
+# Why we do use tuples
+  # - They are more efficient that lists
+  # - We use them for data that shouldn't change
+  # - Some methods return them like dict.items()
+  # - They can be used as keys in a dictionary
+  
