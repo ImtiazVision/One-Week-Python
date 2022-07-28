8af48c60c50d497475420828193e36e556eb1a66
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-28 13:29:50 -0400

    tuple functionalities

--- sets_and_tuples/tuple_functionalities.py
+++ sets_and_tuples/tuple_functionalities.py
@@ -1,6 +1,8 @@
 # To access elements of a tuple, we use index of that element 
 dishes = ('burrito', 'taco', 'quesadilla', 'fajita')
 print(dishes[2])
+# we can also use .index() method to ask for index of an element by providing its name inside the index method
+print(dishes.index('fajita'))
 
 # Slicing of tuple
 print(dishes[0:3]) # 0,1,2 but 3rd element is omitted
