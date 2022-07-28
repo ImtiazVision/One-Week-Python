6be9896bb4ddd9047d5f58c77fb8dc88555f7fff
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-28 19:11:38 -0400

    introduction to sets

--- sets_and_tuples/sets_introduction.py
+++ sets_and_tuples/sets_introduction.py
@@ -1,2 +1,2 @@
-# Sets are unordered collections of data with no duplicate elements
+# Sets are unordered collections of data with no duplicate(unique) elements. Sets can only contain immutable elements. 
 
