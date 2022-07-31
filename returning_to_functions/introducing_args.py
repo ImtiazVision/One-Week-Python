c8c6b1059de5ed31f74b3b36755165282b7d1ef5
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-31 15:16:13 -0400

    *args

--- returning_to_functions/introducing_args.py
+++ returning_to_functions/introducing_args.py
@@ -1,2 +1,7 @@
-# We can use the 'wildcard' or asterisk (*) notation to write functions that accept any number of arguments
+# We can use the 'wildcard' or asterisk (*) notation to write functions that accept any number of arguments.
+
+# an example of '*args' is the min() and max() function as they take 2-infinite number of arguments
+
+print(min(1,3,5,9,2,4,6,7,8,10,-111,-94)) # output : -111 
+print(max(1, 3, 5, 9, 2, 4, 6, 7, 8, 10, -111, -94)) # output: 10
 
