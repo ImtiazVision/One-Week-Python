25e0ae2aa199b3c5bb874ffaba6953ddab528637
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-31 14:03:51 -0400

    set operations

--- sets_and_tuples/set_operations.py
+++ sets_and_tuples/set_operations.py
@@ -1,1 +1,4 @@
 # Intersection: returns new set with numbers common to left and right
+
+
+# Difference (left - right): returns new set with members from left 'not' in right (anything from left set that is not present in right set)
