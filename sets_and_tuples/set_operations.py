3ea753eb544805a61fd48cf925a1f32ac7d8bf17
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-31 14:13:14 -0400

    set operations intersection

--- sets_and_tuples/set_operations.py
+++ sets_and_tuples/set_operations.py
@@ -1,8 +1,12 @@
 # Intersection (left & right): returns new set with numbers common to left and right
-web_dev = {'SQL', 'css', 'html', 'JavaScript'}
-print(web_dev)
+web_dev = {'SQL', 'css', 'html', 'JavaScript', 'Python'}
+# print(web_dev)
 
 data = {'R', 'SQL', 'Python'}
-print(data)
+# print(data)
+
+# Intersection of web_dev & data
+print(web_dev & data)
+
 
 # Difference (left - right): returns new set with members from left 'not' in right (anything from left set that is not present in right set)
