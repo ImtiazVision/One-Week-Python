ee1c1bf8757a0c2436df0790cd47b32ea35b0659
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-07-31 14:49:59 -0400

    set operations union

--- sets_and_tuples/set_operations.py
+++ sets_and_tuples/set_operations.py
@@ -11,4 +11,8 @@ print(web_dev & data)  # Output : {'Python', 'SQL'}
 # Union (left | right): returns new set with members from left and right. Union is represented by a pipe (|) character.
 print(web_dev | data)
 # Output : {'JavaScript', 'Python', 'R', 'css', 'html', 'SQL'}
-# Difference (left - right): returns new set with members from left 'not' in right (anything from left set that is not present in right set)
+
+# Difference (left - right): returns new set with members from left 'not' in right (anything from left set that is not present in right set). Here order matters and whatever set is on the left will be subtracted from the right. 
+
+print(web_dev - data)  # output : {'css', 'html', 'JavaScript'}
+print(data - web_dev)  # output : {'R'}
