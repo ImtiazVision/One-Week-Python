7b0ed7702a764f9b8e911af941dc3f6050fb115f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-01 17:20:36 -0400

    *args in action

--- returning_to_functions/args_in_action.py
+++ returning_to_functions/args_in_action.py
@@ -2,3 +2,14 @@ def count_stuff(*args):
   print(f"You have passed me {len(args)} arguments")
   
 count_stuff(1,2,3,4,5)
+
+
+def add(*nums):
+  total = 0
+  for num in nums:
+    total += num
+  print(total)
+
+add(2,3,5)
+
+
