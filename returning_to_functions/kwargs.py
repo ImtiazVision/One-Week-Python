09da1268436f323baa63db48923d90ad833adb8f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-01 17:29:46 -0400

    **kwargs in action

--- returning_to_functions/kwargs.py
+++ returning_to_functions/kwargs.py
@@ -1,2 +1,2 @@
-# **kwargs: We can use the '**' notation to write functions that accept any number of keyword arguments. 
+# **kwargs: We can use the '**' notation to write functions that accept any number of keyword arguments. This '**kwargs' are applicable to dictionaries. 
 
