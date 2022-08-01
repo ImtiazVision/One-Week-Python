853ab83fc855353985292ce3c65956bb96ef04aa
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-01 14:03:08 -0400

    *args

--- returning_to_functions/introducing_args.py
+++ returning_to_functions/introducing_args.py
@@ -10,5 +10,5 @@ print(max(1, 3, 5, 9, 2, 4, 6, 7, 8, 10, -111, -94)) # output: 10
 def average(*args):
   total = 0
   for arg in args:
-    total += arg
+    total += arg # total = total + arg
   
