dce2ed9111a18f15d8a90c6b4790c6d111f3bca6
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-01 17:35:37 -0400

    **kwargs in action

--- returning_to_functions/kwargs.py
+++ returning_to_functions/kwargs.py
@@ -1,2 +1,8 @@
 # **kwargs: We can use the '**' notation to write functions that accept any number of keyword arguments. This '**kwargs' are applicable to dictionaries. 
 
+def print_ages(**kwargs):
+  for key, value in kwargs.items():
+    print(f"{key} is {value} years old")
+    
+print_ages(John=21, Kim=41, Sam=29)
+
