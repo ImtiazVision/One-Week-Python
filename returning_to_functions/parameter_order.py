75bb3eebced6c4811907ecfa1a60b2b8a44085bd
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-02 10:50:44 -0400

    parameter list order

--- returning_to_functions/parameter_order.py
+++ returning_to_functions/parameter_order.py
@@ -4,9 +4,10 @@
 # (parameters, *args, default parameters, **kwargs)
 
 # here default parameter is: status='married'
-def display_info(person, *args, status="married"):
+def display_info(person, *args, status="married", **kwargs):
   print(f"person is: {person}")
   print(f"status is: {status}")
   print(f"args are : {args}")
+  print(f"kwargs are : {kwargs}")
 
 display_info('Jon', True, False, 2,8,10)
