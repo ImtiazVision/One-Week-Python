eea31316271b6c33fd78c5452d2202c1b3ae5718
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-02 10:35:09 -0400

    parameter list order

--- returning_to_functions/parameter_order.py
+++ returning_to_functions/parameter_order.py
@@ -1,4 +1,10 @@
-# When defining functions, the order of parameters matters! The default argument has to come second after regular parameters.
+# When defining functions, the order of parameters matters! The default argument has to come second after positional arguments/parameters.
 
 # this is the order which parameter follows
 # (parameters, *args, default parameters, **kwargs)
+
+def display_info(person, status="married"):
+  print(f"person is: {person}")
+  print(f"status is: {status}")
+
+display_info('Jon')
