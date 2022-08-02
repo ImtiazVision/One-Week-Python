4b812162f0f3b949eb2d36e1d60148a2c351355b
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-02 10:31:24 -0400

    parameter list order

--- returning_to_functions/parameter_order.py
+++ returning_to_functions/parameter_order.py
@@ -1,1 +1,4 @@
-# When defining functions, the order of parameters matters!
+# When defining functions, the order of parameters matters! The default argument has to come second after regular parameters.
+
+# this is the order which parameter follows
+# (parameters, *args, default parameters, **kwargs)
