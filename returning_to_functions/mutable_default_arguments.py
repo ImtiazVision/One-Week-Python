c9f47e285d367fbe0bda1bb5107d54426674a551
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-02 11:16:48 -0400

    mutable default arguments

--- returning_to_functions/mutable_default_arguments.py
+++ returning_to_functions/mutable_default_arguments.py
@@ -3,6 +3,8 @@
 #   list1.append(value)
 #   print(list1)
 
+# to fix the mutable default parameter 'list1', we set it equal to 'None' and create an empty new list each time function gets called. 
+
 def add_twice(value, list1=None):
   if list1 is None:
     list1 = []
