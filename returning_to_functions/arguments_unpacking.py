60d5f54add6be58786ba1fe4350d5b4761af635d
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-02 16:04:54 -0400

    arguments unpacking

--- returning_to_functions/arguments_unpacking.py
+++ returning_to_functions/arguments_unpacking.py
@@ -1,1 +1,14 @@
 # Argument Unpacking: turning sequences into separate args. 
+
+def average(*args):
+  total = 0
+  for arg in args:
+    total += arg
+  print(total/len(args))
+  
+nums = [8,2,1,3,9,6,7]
+
+#average(nums)  # TypeError
+# Instead, we can 'unpack' the list into individual args using an asterisks. 
+
+average(*nums)
