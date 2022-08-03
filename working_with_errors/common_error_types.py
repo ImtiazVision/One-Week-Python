c976911980af1ddeea942548d098458b7886f6d0
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-03 14:19:47 -0400

    Common Errors

--- working_with_errors/common_error_types.py
+++ working_with_errors/common_error_types.py
@@ -1,2 +1,10 @@
 # Common errors are:
 # KeyError, ValueError, SyntaxError, NameError, IndexError, TypeError
+
+# SyntaxError
+# print(@)  # SyntaxError: invalid syntax
+# print(while a > 9)  # SyntaxError: invalid syntax
+
+# NameError 
+# NameError occurs when we try to access a variable that doesn't exists. 
+# print(asdf)  # NameError: name 'asdf' is not defined
