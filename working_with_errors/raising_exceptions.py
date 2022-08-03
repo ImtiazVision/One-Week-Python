1cea1d44b3e60a9d37587b6db7d8e820ebf89c3b
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-03 15:30:37 -0400

    Common Errors

--- working_with_errors/raising_exceptions.py
+++ working_with_errors/raising_exceptions.py
@@ -1,3 +1,7 @@
-# We can raise our own exceptions (force them to happen) whenever we want, using the 'raise' keyword. 
+# We can raise our own exceptions (force them to happen) whenever we want, using the 'raise' keyword.We can also provide a specific message when raising an exception. raise ValueError('invalid character')
+print('Hello World')
 
-raise KeyError 
+'hello'[6]
+
+
+print("Goodbye")
