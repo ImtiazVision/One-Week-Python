a69cbc949bd90a5b35b2a34c7078e8fbb25e2246
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-03 14:05:32 -0400

    args kwargs problem set exercise

--- returning_to_functions/args_kwargs_problem_set_exercise.py
+++ returning_to_functions/args_kwargs_problem_set_exercise.py
@@ -5,6 +5,9 @@
 
 #  contains_pickle("red", 45, "pickle", [])  --> True
 #  contains_pickle(1,2, "blue") ---------------> False
+from curses.panel import top_panel
+
+
 def contains_pickle(*args):
     # if 'pickle' in args:
     #   return True
@@ -40,5 +43,8 @@ count_fails(45,56,90,60,35,20)
 # get_top_students(colt=61, elton=76) -------------------> []
 # get_top_students(kitty=80, blue=95, toad=91)-----------> ['blue', 'toad']
 
-
+def get_top_students(**kwargs):
+  top_panel_students = []
+  for student, score in kwargs.items():
+    
 
