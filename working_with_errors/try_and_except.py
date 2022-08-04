6ecc3a57b250211990139359d65f6b68e5c0a3d9
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-04 14:42:55 -0400

    try would generate error while except will run if error raised during try section

---
+++ working_with_errors/try_and_except.py
@@ -0,0 +1,8 @@
+# try/except: We can use try and except keywords to handle exceptions. If an exception is raised in the try block, the except block will run.
+
+
+# try:
+#   <code that could generate error>
+# except:
+#   <code that runs if error raised>
+
