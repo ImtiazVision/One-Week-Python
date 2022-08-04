5b2bbf27e602bb8433b58049565a99b9279e1816
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-04 15:26:04 -0400

    try and except

--- working_with_errors/try_and_except.py
+++ working_with_errors/try_and_except.py
@@ -22,6 +22,6 @@ print(f"You have entered {num1}")
 # Usually, it's better to except a specific exception and handle it, rather than handling any possible exception that could occur. 
 try:
   num = int(input('Please enter a number: '))
-except ValueError:
+except ValueError: # here, we are saying only handle ValueError
   print('Oh no, that is not a number!')
 
