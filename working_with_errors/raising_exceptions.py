9cb5fee46fff5dbf209a75b50fe37254e951e00d
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-04 13:03:08 -0400

    raising exceptions

--- working_with_errors/raising_exceptions.py
+++ working_with_errors/raising_exceptions.py
@@ -1,7 +1,7 @@
 # We can raise our own exceptions (force them to happen) whenever we want, using the 'raise' keyword.We can also provide a specific message when raising an exception. raise ValueError('invalid character')
-print('Hello World')
+# print('Hello World')
 
-'hello'[6]
+# 'hello'[6]
 
 
-print("Goodbye")
+# print("Goodbye")
