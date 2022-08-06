68c5d1a7a4071af59f92dcdeddab57609247b7a8
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-06 14:53:34 -0400

    modules introduction

--- modules/module_intro_and_built_in_module.py
+++ modules/module_intro_and_built_in_module.py
@@ -1,3 +1,5 @@
 # Module: A module is simply a Python file that contains code that can be re-used in other files.
 
 # Modules allow us to break up complex programs into smaller, more manageable pieces across multiple files. 
+
+# There are 3 types of modules: 1. Built-in 2. Custom 3. 3rd Party
