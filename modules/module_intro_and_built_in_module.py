86b8c9b0fd3367636da9860067f672ae9c4b182f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-06 13:17:52 -0400

    modules introduction

---
+++ modules/module_intro_and_built_in_module.py
@@ -0,0 +1,3 @@
+# Module: A module is simply a Python file that contains code that can be re-used in other files.
+
+# Modules allow us to break up complex programs into smaller, more manageable pieces across multiple files. 
