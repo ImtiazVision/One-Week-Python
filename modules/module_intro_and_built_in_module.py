7c6dd0c52af8d3acbc915a9ec63c5c10ba6ed100
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-06 14:55:43 -0400

    modules introduction

--- modules/module_intro_and_built_in_module.py
+++ modules/module_intro_and_built_in_module.py
@@ -3,3 +3,6 @@
 # Modules allow us to break up complex programs into smaller, more manageable pieces across multiple files. 
 
 # There are 3 types of modules: 1. Built-in 2. Custom 3. 3rd Party
+
+# Built-in Module: Standard Library
+# Python comes with tons of built-in modules that we can use, if we import them. Each module consists of methods and functionality bundled together. 
