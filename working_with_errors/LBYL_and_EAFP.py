1a08ccf20bbc6afabd3991dd0fe29984ca5d25d3
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-06 12:46:26 -0400

    EAFP: Easier to ask forgiveness than permission

--- working_with_errors/LBYL_and_EAFP.py
+++ working_with_errors/LBYL_and_EAFP.py
@@ -1,6 +1,10 @@
 # LBYL: Look Before You Leap
 # It is a coding style where we explicitly test for pre-conditions before making calls or 'leaping'. It is characterized by lots of if statements.  
 
+num = input("Enter a number: ")
+# 'Look': Check to see if 'num' is numeric
+if num.isnumeric():
+  
 
 
 # EAFP: Easier to ask forgiveness than permission
