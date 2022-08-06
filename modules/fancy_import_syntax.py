90712fe2ab5b8282656ffe5e7896dc0e03a3e354
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-06 16:31:03 -0400

    fancy import syntax like as and from...import

--- modules/fancy_import_syntax.py
+++ modules/fancy_import_syntax.py
@@ -3,7 +3,7 @@
 import random as rand
 print(rand.randint(1,99))
 
-# from...import : Use the 'from <module> import <method>' syntax to import specific pieces of a module 
+# from...import : Use the 'from <module> import <method>' syntax to import specific pieces of a module. This is more specific and cater to specific need. We don't have to download the entire module but download specific method like 'randint' 
 
 from random import randint
 print(randint(99,110))
