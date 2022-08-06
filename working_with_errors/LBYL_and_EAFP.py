c7d9bd912d2c8a91de45b33f1d3bc52863eef15b
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-06 12:28:41 -0400

    EAFP: Easier to ask forgiveness than permission

--- working_with_errors/LBYL_and_EAFP.py
+++ working_with_errors/LBYL_and_EAFP.py
@@ -1,2 +1,6 @@
 # LBYL: Look Before You Leap
 # It is a coding style where we explicitly test for pre-conditions before making calls or 'leaping'. It is characterized by lots of if statements.  
+
+
+
+# EAFP: Easier to ask forgiveness than permission
