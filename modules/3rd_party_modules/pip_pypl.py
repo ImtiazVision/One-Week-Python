7dcc7f83caf5437a87986e226bb04b5ea0f9274c
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-08 12:21:09 -0400

    pypi

--- modules/3rd_party_modules/pip_pypl.py
+++ modules/3rd_party_modules/pip_pypl.py
@@ -1,1 +1,4 @@
 # pip is the Python package installer that we can use to install hundreds of thousands of packages for use in our projects. 
+
+
+# PyPI: The Python Package Index is a repository of software for the Python programming language. It helps us find and install software developed and shared by the Python community. 
