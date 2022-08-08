45ac23b1ae9ab0c2b336ac541db823b02fabef5a
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-08 14:47:35 -0400

    Object Oriented Programming

--- object_oriented_programming/intro_to_OOP.py
+++ object_oriented_programming/intro_to_OOP.py
@@ -1,2 +1,2 @@
-# Object Oriented Programming is an approach to Programming that involves modeling 'things' into Python objects.
+# Object Oriented Programming is an approach to Programming that involves modeling 'things' into Python objects. These objects can contain both data AND functionality all wrapped up together into a reusable component. 
 
