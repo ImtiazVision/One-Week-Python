8a477791e3db5cdf02c610d2433ae7b1649dda56
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-08 15:11:26 -0400

    Object Oriented Programming

--- object_oriented_programming/intro_to_OOP.py
+++ object_oriented_programming/intro_to_OOP.py
@@ -1,2 +1,6 @@
-# Object Oriented Programming is an approach to Programming that involves modeling 'things' into Python objects. These objects can contain both data AND functionality all wrapped up together into a reusable component. 
+# Object Oriented Programming is an approach to Programming that involves modeling 'things' into Python objects. These objects can contain both data AND functionality all wrapped up together into a reusable component.
+
+# For example, we we want to create a chess game, we need to have reusable components such as  match, AIPlayer, Player, Piece, Board etc. 
+
+# Class: Classes are blueprints or recipes that we can later use to create objects from. A class describes what properties and functionality individual objects will contain. 
 
