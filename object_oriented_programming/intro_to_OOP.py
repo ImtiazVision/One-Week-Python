b48e8dff1167494f2edd2a3aa65820ac56370b3f
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-09 09:07:26 -0400

    Object Oriented Programming

--- object_oriented_programming/intro_to_OOP.py
+++ object_oriented_programming/intro_to_OOP.py
@@ -8,4 +8,5 @@
 
 # Example of classes: 
 # Cupcake recipe: color, frosting, flavor, flour type, recipe instructions etc. all are unique(blueprint) for this cupcake recipe.
+# Instances: we can follow the basic rules from the class blueprint but however, we can instantiate them into different color,size, shape of cupcakes. 
 
