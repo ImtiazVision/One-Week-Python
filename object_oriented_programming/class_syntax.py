6e2457dbc49d3ead7632d6b19c092d7cb4417621
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-10 18:22:00 -0400

    class syntax

--- object_oriented_programming/class_syntax.py
+++ object_oriented_programming/class_syntax.py
@@ -1,6 +1,8 @@
 # To define a class, first thing we need is the 'class' keyword and class name is Capitalized.
 
 # __init__ is a special method that is automatically called whenever a new class i.e. Puppy is created(instantiated). 
+
+# self: the 'self' keyword refers to the 'current' instance of the class i.e. Puppy. 
 class Puppy:
   def __init__(self, name):
     self.name = name
