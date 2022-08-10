6203e635cec5a7131f7010628089c40ad8f67fea
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-10 18:23:23 -0400

    class syntax

--- object_oriented_programming/class_syntax.py
+++ object_oriented_programming/class_syntax.py
@@ -2,7 +2,7 @@
 
 # __init__ is a special method that is automatically called whenever a new class i.e. Puppy is created(instantiated). 
 
-# self: the 'self' keyword refers to the 'current' instance of the class i.e. Puppy. 
+# self: the 'self' keyword refers to the 'current' instance of the class i.e. Puppy. The 'self' must be the first parameter to '__init__' 
 class Puppy:
   def __init__(self, name):
     self.name = name
