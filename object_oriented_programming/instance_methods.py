e26da3e0375e4d1b8c49b9f67af3058deee6e4ad
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-11 13:16:27 -0400

    instance methods

--- object_oriented_programming/instance_methods.py
+++ object_oriented_programming/instance_methods.py
@@ -1,1 +1,10 @@
 # We can add a method to a class. Lets see that in action.
+
+class Puppy:
+  def __init__(self, name):
+    self.name = name
+    self.tricks = []
+    
+# this add_trick() method appends a new trick to a Puppy instance's tricks list:
+  def add_trick(self, new_trick):
+    self.tricks.append(new_trick)
