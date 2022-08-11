7ae44aeeb17ccc197719a9c914dcec2476ee3d50
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-11 15:20:49 -0400

    instance methods

--- object_oriented_programming/practicing_instance_method.py
+++ object_oriented_programming/practicing_instance_method.py
@@ -5,3 +5,9 @@ class Puppy:
     
   def add_trick(self, new_trick):
     self.tricks.append(new_trick)
+    
+  def perform_trick(self, trick): # adding another instance method 
+    if trick in self.tricks:
+      print(f"{self.name} performs {trick}")
+    else:
+      print(f"{self.name} does not know {trick}")
