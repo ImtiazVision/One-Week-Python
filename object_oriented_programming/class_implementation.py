e74c1b90f964cf68213fa3891c5e34ab3d2ec234
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-11 11:12:29 -0400

    class implementation

--- object_oriented_programming/class_implementation.py
+++ object_oriented_programming/class_implementation.py
@@ -1,5 +1,6 @@
 class Dog:
-  def __init__(self, name, breed):
+  def __init__(self, name, breed, location):
     self.name = name
     self.breed = breed
+    self.location = location
     self.tricks = []
