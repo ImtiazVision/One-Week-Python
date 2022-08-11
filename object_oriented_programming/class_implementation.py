75599c4d99823716d6ffe9f026558f76774be9d8
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-11 11:17:07 -0400

    class implementation

--- object_oriented_programming/class_implementation.py
+++ object_oriented_programming/class_implementation.py
@@ -4,3 +4,8 @@ class Dog:
     self.breed = breed
     self.location = location
     self.tricks = []
+    
+german = Dog('Elmo','German Shepard', 10001)
+print(german.name)
+print(german.breed)
+print(german.location)
