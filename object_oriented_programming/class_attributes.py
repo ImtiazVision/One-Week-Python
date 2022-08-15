e0bdd8c834a1bafa6fd1c7b448433b87458e1da8
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-15 13:47:52 -0400

    class attributes

--- object_oriented_programming/class_attributes.py
+++ object_oriented_programming/class_attributes.py
@@ -1,6 +1,6 @@
 class Puppy:
   
-  species = 'canine' # 'species' is defined on the class itself. All instances of Puppy will have the same value for 'species'.
+  species = 'canine' # 'species' is defined on the class itself. All instances of Puppy will have the same value for 'species'. class attributes are shared across all the instances of a parent class. 
   
   def __init__(self, name):
     self.name = name
