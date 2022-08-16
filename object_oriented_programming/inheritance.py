838620e67d20672e15ade6c886d832d7fccf0aab
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-16 13:25:36 -0400

    inheritence

---
+++ object_oriented_programming/inheritance.py
@@ -0,0 +1,11 @@
+class Cat:
+  def __init__(self,name):
+    self.name = name
+  def meow(self):
+    print(f"{self.name} meows")
+    
+# The following Lion class inherits from the base class Cat on line 9.
+
+class Lion(Cat):
+  def roar(self):
+    print(f"{self.name} roars")
