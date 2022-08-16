553f3be9efeeaa92f32f306eb864686a7fc96613
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-08-16 13:51:31 -0400

    inheritence super method to access functionalities/methods from the parent/base class

--- object_oriented_programming/super_function.py
+++ object_oriented_programming/super_function.py
@@ -10,7 +10,8 @@ class Cat:
 class Lion(Cat):
   
   def __init__(self, name, pride_name):
-    super().__init__(name)
+    super().__init__(name) # here we are accessing parent/base class's init 
     self.pride_name = pride_name
+    
   def roar(self):
     print(f"{self.name} roars")
